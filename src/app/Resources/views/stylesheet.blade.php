a91de5f180dfc79e0679e47907909c9017c38879
Andy <andy-saw@hotmail.com>
2017-12-16 19:53:51 +0900

    Create alert, badge, blockquote, typography components

--- src/app/Resources/views/stylesheet.blade.php
+++ src/app/Resources/views/stylesheet.blade.php
@@ -19,115 +19,204 @@
         <style>
             body {
                 padding: 1em;
+                background: #fff;
+            }
+
+            h1 {
+                margin-top: 1em;
             }
 
             .card,
             .progressbar,
-            .button {
-                margin-bottom: 0.5em
+            .button,
+            .alert {
+                margin-bottom: 1em
             }
 
         </style>
     </head>
     <body>
-        
-        Headers
-        <h1>Header 1</h1>
-        <h2>Header 2</h2>
-        <h3>Header 3</h3>
-        <h4>Header 4</h4>
-        <h5>Header 5</h5>
-        <h6>Header 6</h6>
 
-        Cards
-        <div class="card">
-            Basic (card)
-        </div>
+        <div class="container">
+            <h1>Overview of PCB's style</h1>
+            <p>
+                This time we're trying for a brighter, more toony style, where components look almost like they're blocks made of matte plastic.
+                This page is merely a showcase of the components created so far that, when combined, make up PCB's design.
+            </p>
 
-        <div class="card primary">
-            Primary (card.primary)
-        </div>
+            <h1>Header 1</h1>
+            <h2>Header 2</h2>
+            <h3>Header 3</h3>
+            <h4>Header 4</h4>
+            <h5>Header 5</h5>
+            <h6>Header 6</h6>
 
-        <div class="card secondary">
-            Secondary (card.secondary)
-        </div>
+            <h1>Typography</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tincidunt felis, at rutrum tortor. Donec at cursus nulla. Curabitur tellus magna, consequat ut feugiat a, viverra ac tortor. Quisque sed pharetra orci. Nam a lorem quis magna mattis pretium. Curabitur consectetur nibh sed eros pulvinar, non mollis nulla feugiat. Sed fringilla dapibus mollis.
+            </p>
+            <p>
+                Sed a urna tempus, pharetra ante sed, suscipit felis. Nulla condimentum consequat ante finibus cursus. Vestibulum sodales a felis vel eleifend. Nam dignissim, magna in lacinia venenatis, turpis sapien faucibus nulla, id euismod urna urna id turpis.
+                <br />
+                <br />
+                Text with <strong>emphasis</strong>.<br />
+                Text with <i>italics</i>.<br />
+                Text with a <a href="#">link</a> to somewhere.<br />
+                <small>Fine print text</small><br />
+            </p>
 
-        <div class="card accent">
-            Accent (card.accent)
-        </div>
+            <h1>Blockquotes</h1>
+            <blockquote>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere tincidunt felis, at rutrum tortor. Donec at cursus nulla.
+            </blockquote>
 
-        <div class="card centered">
-            Centered (card.centered)
-        </div>
 
-        Progress Bars
-        <div class="card primary">
+            <h1>Badges</h1>
+            <span class="badge">Basic</span>
+            <span class="badge primary">Primary</span>
+            <span class="badge secondary">Secondary</span>
+            <span class="badge accent">Accent</span>
 
-            <div class="progressbar primary">
-                <div class="outer">
-                    <div class="inner" style="width:25%"></div>
+            <h1>Cards</h1>
+            <div class="card">
+                <div class="card-body">
+                    Basic (card)
+                </div>
+            </div>
+
+            <div class="card primary">
+                <div class="card-body">
+                    Primary (card.primary)
+                </div>
+            </div>
+
+            <div class="card secondary">
+                 <div class="card-body">
+                    Secondary (card.secondary)
+                </div>
+            </div>
+
+            <div class="card accent">
+                 <div class="card-body">
+                    Accent (card.accent)
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-header">
+                    Header
+                </div>
+                <div class="card-body">
+                    Basic (card)
+                </div>
+            </div>
+
+            <div class="card primary">
+                <div class="card-header">
+                    Header
+                </div>
+                <div class="card-body">
+                    Primary (card.primary)
+                </div>
+            </div>
+
+            <div class="card secondary">
+                <div class="card-header">
+                    Header
+                </div>
+                <div class="card-body">
+                    Secondary (card.secondary)
+                </div>
+            </div>
+
+            <div class="card accent">
+                <div class="card-header">
+                    Header
+                </div>
+                <div class="card-body">
+                    Accent (card.accent)
+                </div>
+            </div>
+
+            <h1>Alerts</h1>
+            <div class="alert warning">
+                <h3><i class="fas fa-exclamation-circle"></i> Warning</h3>
+                <p>You have <a href="#">5 unchecked</a> player reports.</p>
+            </div>
+            <div class="alert error">
+                <h3><i class="fas fa-exclamation-circle"></i> Error</h3>
+                <p>Image size exceeded the limit. Please upload an image less than <strong>1mb</strong>.</p>
+            </div>
+            <div class="alert success">
+                <h3><i class="fas fa-check"></i> Success</h3>
+                <p>Ban appeal submitted succesfully!</p>
+            </div>
+
+            <h1>Progress Bars</h1>
+            <div class="card primary">
+                <div class="card-body">
+
+                    <div class="progressbar primary">
+                        <div class="outer">
+                            <div class="inner" style="width:25%"></div>
+                        </div>
+                    </div>
+                    
+                    <div class="progressbar secondary">
+                        <div class="outer">
+                            <div class="inner" style="width:25%"></div>
+                        </div>
+                    </div>
+
+                    <div class="progressbar accent small">
+                        <div class="outer">
+                            <div class="inner" style="width:25%"></div>
+                        </div>
+                    </div>
+
+                    <div class="progressbar accent">
+                        <div class="outer">
+                            <div class="inner" style="width:66%">66%</div>
+                            <div class="label">33%</div>
+                        </div>
+                    </div>
+                    
+                    <div class="progressbar accent">
+                        <div class="outer">
+                            <div class="inner" style="width:25%"></div>
+                        </div>
+                        <div class="markers">
+                            <span>0</span>
+                            <span>250</span>
+                            <span>500</span>
+                            <span>750</span>
+                            <span>1000</span>
+                        </div>
+                    </div>
+
                 </div>
             </div>
             
-            <div class="progressbar secondary">
-                <div class="outer">
-                    <div class="inner" style="width:25%"></div>
-                </div>
-            </div>
 
-            <div class="progressbar accent small">
-                <div class="outer">
-                    <div class="inner" style="width:25%"></div>
-                </div>
-            </div>
+            <h1>Buttons</h1>
+            <a class="button primary" href="#">Link Button</a>
+            <a class="button secondary" href="#">Link Button</a>
+            <a class="button accent" href="#">Link Button</a>
 
-            <div class="progressbar secondary bordered">
-                <div class="outer">
-                    <div class="inner" style="width:25%"></div>
-                </div>
-            </div>
+            <a class="button accent" href="#">
+                <i class="fas fa-lock"></i>
+                Link with Icon
+            </a>
 
-            <div class="progressbar accent">
-                <div class="outer">
-                    <div class="inner" style="width:66%">66%</div>
-                    <div class="label">33%</div>
-                </div>
-            </div>
+            <a class="button primary disabled" href="#">Disabled Button</a>
+            <a class="button secondary disabled" href="#">Disabled Button</a>
+            <a class="button accent disabled" href="#">Disabled Button</a>
             
-            <div class="progressbar accent">
-                <div class="outer">
-                    <div class="inner" style="width:25%"></div>
-                </div>
-                <div class="markers">
-                    <span>0</span>
-                    <span>250</span>
-                    <span>500</span>
-                    <span>750</span>
-                    <span>1000</span>
-                </div>
-            </div>
+            <a class="button accent large" href="#">Large Button</a>
 
+            <a class="button secondary fill" href="#">Fill Button</a>
+        
         </div>
-        
-
-        Buttons
-        <a class="button primary" href="#">Link Button</a>
-        <a class="button secondary" href="#">Link Button</a>
-        <a class="button accent" href="#">Link Button</a>
-
-        <a class="button accent" href="#">
-            <i class="fas fa-lock"></i>
-            Link with Icon
-        </a>
-
-        <a class="button primary disabled" href="#">Disabled Button</a>
-        <a class="button secondary disabled" href="#">Disabled Button</a>
-        <a class="button accent disabled" href="#">Disabled Button</a>
-        
-        <a class="button accent large" href="#">Large Button</a>
-
-        <a class="button secondary fill" href="#">Fill Button</a>
-        
 
         <script src="{{ mix('assets/js/app.js') }}"></script>
         
