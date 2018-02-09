dc73d3ac9b28c00cb9a0ce4d211fca9a346a02a8
Andy <andy-saw@hotmail.com>
2018-02-09 23:59:53 +0900

    Re-enable css url processing

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -3,9 +3,9 @@ let mix = require('laravel-mix');
 
 // there seems to be a bug causing compile time to skyrocket when css url 
 // processing is enabled. re-enable this when it's fixed
-mix.options({ 
-    processCssUrls: false 
-});
+// mix.options({ 
+//     processCssUrls: false 
+// });
 
 mix.typeScript('assets/js/app.ts', 'public/assets/js')
    .sass('assets/sass/app.scss', 'public/assets/css')
