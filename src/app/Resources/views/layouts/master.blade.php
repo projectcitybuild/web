3e6f9052ff552da7741c40b012d6595ed8e929a1
Andy <andy-saw@hotmail.com>
2018-02-09 23:32:41 +0900

    Improve master template meta tags

--- src/app/Resources/views/layouts/master.blade.php
+++ src/app/Resources/views/layouts/master.blade.php
@@ -1,15 +1,28 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-    <head>
+    <head>    
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="google-site-verification" content="Sp9E55tVkNph_ttvggLD52MY-ACeGfeivQbmWp7CWfo" />
+        <meta name="google-site-verification" content="Sp9E55tVkNph_ttvggLD52MY-ACeGfeivQbmWp7CWfo">
+        <meta name="description" content="@yield('description')">
 
-        <title>Project City Build</title>
+        <meta property="og:url"         content="https://www.projectcitybuild.com">
+        <meta property="og:title"       content="@yield('title', 'Project City Build')">
+        <meta property="og:description" content="@yield('description')">
+        <meta property="og:site_name"   content="Project City Build">
+        <meta property="og:type"        content="website">
+        <meta property="og:locale"      content="en_US">
 
-        <link rel="stylesheet" href="{{ mix('assets/css/app.css') }}" />
+        <meta name="twitter:card"       content="@yield('description')">
+        <meta name="twitter:site"       content="@PCB_Minecraft">
+
+
+        <title>@yield('title', 'Project City Build')</title>
+
+        <link rel="stylesheet" href="{{ mix('assets/css/app.css') }}">
+        <link rel="icon" type="type/x-icon" href="{{ asset('assets/favicon.ico') }}">
         
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/brands.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/solid.js"></script>
