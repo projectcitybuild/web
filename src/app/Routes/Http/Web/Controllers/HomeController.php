86581251f635bda06b4d023cd2311f3e20a881e5
Andy <andy-saw@hotmail.com>
2018-02-25 01:39:12 +0900

    Remove old forum announcement fetch logic

--- src/app/Routes/Http/Web/Controllers/HomeController.php
+++ src/app/Routes/Http/Web/Controllers/HomeController.php
@@ -5,35 +5,22 @@ namespace App\Routes\Http\Web\Controllers;
 use Illuminate\Support\Facades\View;
 use App\Modules\Donations\Services\DonationStatsService;
 use App\Routes\Http\Web\WebController;
-use App\Modules\Forums\Services\Retrieve\OfflineRetrieve;
 
-class HomeController extends WebController
-{
-    /**
-     * @var ForumRetrieveInterface
-     */
-    private $forumRetrieveService;
-
+class HomeController extends WebController {
     /**
      * @var DonationStatsService
      */
     private $donationStatsService;
 
     
-    public function __construct(
-        OfflineRetrieve $forumRetrieveService, 
-        DonationStatsService $donationStatsService
-    ) {
-        $this->forumRetrieveService = $forumRetrieveService;
+    public function __construct(DonationStatsService $donationStatsService) {
         $this->donationStatsService = $donationStatsService;
     }
 
     public function getView() {
-        $announcements = $this->forumRetrieveService->getAnnouncements([]);
         $donations = $this->donationStatsService->getAnnualPercentageStats();
 
         return view('home', [
-            'announcements' => $announcements,
             'donations'     => $donations,
         ]);
     }
--- src/assets/views/home.blade.php
+++ src/assets/views/home.blade.php
@@ -4,45 +4,5 @@
 @section('description', 'Medium size Minecraft community active since 2010. We host multiple servers and maps, including Survival, Creative, as well as maps dedicated to projects such as our signature Big City map. With over 14,000 registrants, PCB has a bit of something for everyone.')
 
 @section('left')
-
     <div id="announcements"></div>
-    @if(isset($announcements))
-        @foreach($announcements as $announcement)
-            <article class="article card">
-                <div class="article__container">
-                    <h2 class="article__heading">{{ $announcement->firstPost->subject }}</h2>
-                    <div class="article__date">{{ $announcement->firstPost->poster_time->format('D, jS \o\f F, Y') }}</div>
-
-                    <div class="article__body">
-                        {!! $announcement->firstPost->body !!}
-                    </div>
-
-                    <div class="article__author">
-                        Posted by
-                        <img src="https://minotar.net/helm/{{ $announcement->poster->real_name }}/16" width="16" alt="{{ $announcement->poster->real_name }}" />
-                        <a href="#">{{ $announcement->poster->real_name }}</a>
-                    </div>
-                </div>
-                <div class="article__footer">
-                    <div class="stats-container">
-                        <div class="stat">
-                            <span class="stat__figure">{{ $announcement->num_replies }}</span>
-                            <span class="stat__heading">Comments</span>
-                        </div>
-                        <div class="stat">
-                            <span class="stat__figure">{{ $announcement->num_views }}</span>
-                            <span class="stat__heading">Post Views</span>
-                        </div>
-                    </div>
-                    <div class="actions">
-                        <a class="button button--accent button--large" href="http://projectcitybuild.com/forums/index.php?topic={{ $announcement->id_topic}}">
-                            Read Post
-                            <i class="fa fa-chevron-right"></i>
-                        </a>
-                    </div>
-                </div>
-            </article>
-        @endforeach
-    @endif
-    
 @endsection
