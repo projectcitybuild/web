02aa86bf9ff8d487ef0275c8d3526fff5d4afaf3
Andy <andy-saw@hotmail.com>
2018-04-22 18:38:35 +0900

    Create donation statistics page

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -3,13 +3,20 @@ let mix = require('laravel-mix');
 
 mix.typeScript('assets/js/app.tsx', 'public/assets/js')
    .sass('assets/sass/app.scss', 'public/assets/css')
-   .version();
 
-mix.browserSync({
-    proxy: '192.168.99.100',
-    files: [
-        'assets/**/*.php',
-        'public/**/*.css', 
-        'public/**/*.js',    
-    ]
-});
+if(mix.config.production) {
+    console.log('test');
+    mix.minify()
+       .version();
+}
+
+else {
+    mix.browserSync({
+        proxy: '192.168.99.100',
+        files: [
+            'assets/**/*.php',
+            'public/**/*.css', 
+            'public/**/*.js',    
+        ]
+    });
+}
