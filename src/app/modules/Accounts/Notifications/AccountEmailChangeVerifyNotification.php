1b314b8e95fb3b6fabb22eb9940768a465266dac
Andy <andy-saw@hotmail.com>
2018-07-04 00:00:44 +0900

    Update email contents

--- src/app/modules/Accounts/Notifications/AccountEmailChangeVerifyNotification.php
+++ src/app/modules/Accounts/Notifications/AccountEmailChangeVerifyNotification.php
@@ -61,12 +61,12 @@ class AccountEmailChangeVerifyNotification extends Notification
             ->subject('Email Address Change was Requested')
             ->from('no-reply@projectcitybuild.com');
 
-        if ($this->isOldEmailAddress === false) {
+        if ($this->isOldEmailAddress === true) {
             $message
                 ->greeting('Email Change Request')
-                ->line('You or somebody else has requested to change their email address to this address. Use the below link if you wish to proceed.')
-                ->action('Yes, use this email address', $this->confirmLink)
-                ->line('If you did not request this please ignore this email.')
+                ->line('You or somebody else has requested to change your account email address. Use the below link if you wish to proceed.')
+                ->action('Yes, proceed with the change', $this->confirmLink)
+                ->line('If you did not request this, please let us know immediately as your account has likely been compromised.')
                 ->line('The above link will expire in 15 minutes.');
         } else {
             $message
