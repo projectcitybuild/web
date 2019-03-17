019ac1ade4e00ec8a4a4399e5e55e351186a20c9
andyksaw <andy-saw@hotmail.com>
2019-03-17 19:20:08 +0900

    Add dependency registry

---
+++ src/application/providers/DependencyContainerProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Application\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DependencyContainerProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot() {}
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register() 
+    {
+        
+    }
+}
