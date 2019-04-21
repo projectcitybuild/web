ba3e040a55fd61bd81d5bea8801640597861c402
andyksaw <andy-saw@hotmail.com>
2019-04-21 19:06:02 +0900

    Migrate majority to laravel structure

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -1,19 +1,27 @@
 <?php
-
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Interfaces\Console\Commands\QueryServerCommand;
+use Application\Environment;
+use Interfaces\Console\Commands\ImportGroupCommand;
+use Interfaces\Console\Commands\GroupAddUserCommand;
+use Interfaces\Console\Commands\GroupRemoveUserCommand;
 
 class Kernel extends ConsoleKernel
 {
+
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
      */
     protected $commands = [
-        //
+        QueryServerCommand::class,
+        ImportGroupCommand::class,
+        GroupAddUserCommand::class,
+        GroupRemoveUserCommand::class,
     ];
 
     /**
@@ -24,19 +32,27 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        if (Environment::isDev()) {
+            return;
+        }
+        
+        $schedule->command('query:status --all')
+                 ->everyFiveMinutes();
+
+        $schedule->command('cleanup:password-reset')
+                ->weekly();
+
+        $schedule->command('cleanup:unactivated-account')
+                ->daily();
     }
 
     /**
-     * Register the commands for the application.
+     * Register the Closure based commands for the application.
      *
      * @return void
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        require base_path('interfaces/console/Routes.php');
     }
 }
--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Entities\Players\Models\MinecraftPlayer;
+use Illuminate\Support\Facades\View;
+use Entities\Donations\Models\Donation;
+use Entities\Payments\AccountPaymentType;
+use Interfaces\Web\Composers\MasterViewComposer;
+use Entities\GamePlayerType;
+use Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +31,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        
+        // we don't want to store namespaces
+        // in the database so we'll map them
+        // to unique keys instead
+        Relation::morphMap([
+            'minecraft_player' => MinecraftPlayer::class,
+            AccountPaymentType::Donation => Donation::class,
+            GamePlayerType::Minecraft => MinecraftPlayer::class,
+        ]);
+
+        // bind the master view composer to the master view template
+        View::composer('front.layouts.master', MasterViewComposer::class);
     }
 }
