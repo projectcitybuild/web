9169a4f6efae6c34e86322d0ef0ac769699f357c
Andy <andy-saw@hotmail.com>
2019-04-28 23:58:35 +0900

    Remove OAuth settings page link

--- src/resources/views/front/pages/account/components/account-sidebar.blade.php
+++ src/resources/views/front/pages/account/components/account-sidebar.blade.php
@@ -11,15 +11,6 @@
                     Account Security
                 </a>
             </li>
-            <li>
-                <a href="{{ route('front.account.social') }}">
-                    <span class="fa-stack fa-2x">
-                        <i class="fas fa-square fa-stack-2x"></i>
-                        <i class="fab fa-facebook fa-stack-1x fa-inverse"></i>
-                    </span>
-                    Connected Social Accounts
-                </a>
-            </li>
         </ul>
 
     </div>
