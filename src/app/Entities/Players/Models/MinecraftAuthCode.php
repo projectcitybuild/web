4d72a53558c211be1f7a7c1f34ca64cd818b967d
Andy <andy-saw@hotmail.com>
2019-05-26 18:30:01 +0900

    Create model

---
+++ src/app/Entities/Players/Models/MinecraftAuthCode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entities\Players\Models;
+
+use App\Model;
+
+final class MinecraftAuthCode extends Model
+{
+    protected $table = 'minecraft_auth_codes';
+
+    protected $primaryKey = 'minecraft_auth_code_id';
+
+    protected $fillable = [
+        'uuid',
+        'minecraft_player_id',
+        'expires_at',
+    ];
+
+    protected $dates = [
+        'expires_at',
+    ];
+
+}
