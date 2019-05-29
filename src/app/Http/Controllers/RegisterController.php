ef8eaa6a771f4ca1fecb8b87dff05efbf6448d0f
Andy <andy-saw@hotmail.com>
2019-05-30 00:37:02 +0900

    Make Discourse login request on behalf of the user after registering

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -6,16 +6,19 @@ use App\Entities\Accounts\Repositories\AccountRepository;
 use App\Entities\Accounts\Repositories\UnactivatedAccountRepository;
 use App\Entities\Accounts\Notifications\AccountActivationNotification;
 use App\Http\Requests\RegisterRequest;
-use Illuminate\Database\Connection;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Carbon\Carbon;
 use Hash;
 use App\Http\WebController;
+use App\Entities\Accounts\Models\Account;
+use Illuminate\Support\Facades\DB;
+use App\Library\Discourse\Authentication\DiscourseLoginHandler;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
-class RegisterController extends WebController
+final class RegisterController extends WebController
 {
-
     /**
      * @var AccountRepository
      */
@@ -27,19 +30,26 @@ class RegisterController extends WebController
     private $unactivatedAccountRepository;
 
     /**
-     * @var Connection
+     * @var DiscourseLoginHandler
      */
-    private $connection;
+    private $discourseLoginHandler;
+
+    /**
+     * @var CLient
+     */
+    private $client;
 
 
     public function __construct(
         AccountRepository $accountRepository,
         UnactivatedAccountRepository $unactivatedAccountRepository,
-        Connection $connection
+        DiscourseLoginHandler $discourseLoginHandler,
+        Client $client
     ) {
         $this->accountRepository = $accountRepository;
         $this->unactivatedAccountRepository = $unactivatedAccountRepository;
-        $this->connection = $connection;
+        $this->discourseLoginHandler = $discourseLoginHandler;
+        $this->client = $client;
     }
 
     public function showRegisterView()
@@ -62,7 +72,8 @@ class RegisterController extends WebController
     }
 
     /**
-     * Attempts to activate an account via token
+     * Finishes the registration process by attempting to activate the account that 
+     * belongs to the given email address
      *
      * @param Request $request
      *
@@ -71,40 +82,83 @@ class RegisterController extends WebController
      */
     public function activate(Request $request)
     {
+        // payload is signed to prevent tampering, so this URL parameter is safe to use as it is
         $email = $request->get('email');
-        if (empty($email)) {
+        
+        if (empty($email)) 
+        {
             return view('front.pages.register.register');
         }
 
         $unactivatedAccount = $this->unactivatedAccountRepository->getByEmail($email);
-        if ($unactivatedAccount === null) {
+        if ($unactivatedAccount === null) 
+        {
             // TODO: inform user that account does not exist
-            abort(404);
+            abort(404, 'Account does not exist');
         }
 
-        $accountByEmail = $this->accountRepository->getByEmail($email);
-        if ($accountByEmail) {
+        $account = $this->accountRepository->getByEmail($email);
+        if ($account) 
+        {
             // TODO: inform user account is already activated
-            abort(410);
+            abort(410, 'Account already activated');
         }
 
-        $this->connection->beginTransaction();
-        try {
-            $this->accountRepository->create(
+        DB::beginTransaction();
+        try 
+        {
+            $account = $this->accountRepository->create(
                 $unactivatedAccount->email,
                 $unactivatedAccount->password,
                 $request->ip(),
                 Carbon::now()
             );
-
             $unactivatedAccount->delete();
 
-            $this->connection->commit();
-        } catch (\Exception $e) {
-            $this->connection->rollBack();
+            DB::commit();
+        } 
+        catch (\Exception $e) 
+        {
+            DB::rollBack();
             throw $e;
         }
 
+        // Login to Discourse on behalf of the user, server-side, to force Discourse to create an account
+        // for the user and therefore stay in sync with PCB. This step is necessary because some users 
+        // register an account but never login once afterwards, which means they'l have a PCB account but 
+        // not a forum account, and this causes all sorts of nasty problems later
+        $this->forceLoginServerSide($request, $account);
+
         return view('front.pages.register.register-verify-complete');
+    }
+
+    /**
+     * Logs-in to Discourse on behalf of the user, server-side
+     *
+     * @param Request $request
+     * @param Account $account
+     * @return void
+     */
+    private function forceLoginServerSide(Request $request, Account $account)
+    {
+        if (Environment::isDev())
+        {
+            return;
+        }
+        try 
+        {
+            $endpoint = $this->discourseLoginHandler->getRedirectUrl(
+                $request,
+                $account->getKey(), 
+                $account->email
+            );
+            
+            $this->client->get($endpoint);
+        } 
+        catch (BadSSOPayloadException $e) 
+        {
+            Log::debug('Missing nonce or return key in session', ['session' => $request->session()]);
+            throw $e;
+        }
     }
 }
