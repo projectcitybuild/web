fa487db15a0262d1f029c8d6dc1ec4eefc33021c
andyksaw <andy-saw@hotmail.com>
2019-07-28 03:38:27 +0900

    Create account email update action

---
+++ src/app/Http/Actions/AccountSettings/UpdateAccountEmail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Actions\AccountSettings;
+
+use Illuminate\Support\Facades\DB;
+use App\Entities\Accounts\Models\Account;
+use App\Library\Discourse\Entities\DiscoursePayload;
+use App\Library\Discourse\Api\DiscourseAdminApi;
+use App\Entities\Accounts\Models\AccountEmailChange;
+
+final class UpdateAccountEmail
+{
+    private $discourseAdminApi;
+
+    public function __construct(DiscourseAdminApi $discourseAdminApi)
+    {
+        $this->discourseAdminApi = $discourseAdminApi;
+    }
+
+    public function execute(Account $account, AccountEmailChange $emailChangeRequest)
+    {
+        $newEmailAddress = $emailChangeRequest->email_new;
+
+        if (empty($newEmailAddress)) {
+            throw new \Exception('New email address cannot be empty');
+        }
+
+        DB::beginTransaction();
+        try {
+            // Notify Discourse that the user's email address has changed
+            $this->updateDiscourseAccount($account->getKey(), $newEmailAddress);
+
+            $account->email = $newEmailAddress;
+            $account->save();
+
+            $emailChangeRequest->delete();
+
+            DB::commit();
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    private function updateDiscourseAccount(int $pcbAccountId, string $newEmailAddress)
+    {
+        $payload = (new DiscoursePayload)
+            ->setPcbId($pcbAccountId)
+            ->setEmail($newEmailAddress);
+
+        try {
+            $this->discourseAdminApi->requestSSOSync($payload->build());
+
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
+            // Sometimes the API fails because the 'requires_activation' key is needed at random in
+            // the payload. As a workaround we'll send the request again but with the key included
+            // this time
+            $payload->requiresActivation(false);
+            $this->discourseAdminApi->requestSSOSync($payload->build());
+        }
+    }
+}
--- src/app/Http/Controllers/AccountSettingController.php
+++ src/app/Http/Controllers/AccountSettingController.php
@@ -8,30 +8,12 @@ use App\Http\Requests\AccountChangePasswordRequest;
 use App\Http\Actions\AccountSettings\UpdateAccountPassword;
 use App\Http\Actions\AccountSettings\SendEmailForAccountEmailChange;
 use App\Http\WebController;
-use App\Library\Discourse\Api\DiscourseAdminApi;
-use App\Library\Discourse\Entities\DiscoursePayload;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Http\Actions\AccountSettings\UpdateAccountEmail;
 
 final class AccountSettingController extends WebController
 {
-    /**
-     * @var DiscourseAdminApi
-     */
-    private $discourseApi;
-
-    /**
-     * @var AccountEmailChangeRepository
-     */
-    private $emailChangeRepository;
-
-
-    public function __construct(DiscourseAdminApi $discourseApi, AccountEmailChangeRepository $emailChangeRepository) {
-        $this->discourseApi = $discourseApi;
-        $this->emailChangeRepository = $emailChangeRepository;
-    }
-
     public function showView()
     {
         return view('front.pages.account.account-settings');
@@ -52,99 +34,63 @@ final class AccountSettingController extends WebController
     }
 
     /**
-     * Shows and updates the 'email change' status view
+     * Either shows information about the current stage in the email change process,
+     * or completes the email change process if the user has just finished verifying
+     * they own both email addresses (current and new)
      *
      * @param Request $request
      * @return View
      */
-    public function showConfirmForm(Request $request)
+    public function showConfirmForm(Request $request, AccountEmailChangeRepository $emailChangeRepository, UpdateAccountEmail $updateAccountEmail)
     {
         $token = $request->get('token');
         $email = $request->get('email');
         
         if (empty($token) || empty($email)) {
-            // if the signed url was valid and yet the email or token field is missing,
-            // something has definitely gone wrong
-            throw new \Exception('Email address or token missing in url parameters');
+            // If the email or token field is missing, the user has highly likely 
+            // tampered with the URL
+            throw new \Exception('Email address and/or token missing');
         }
 
-        $changeRequest = $this->emailChangeRepository->getByToken($token);
-        
+        $changeRequest = $emailChangeRepository->getByToken($token);
         if ($changeRequest === null) {
-            // token has either expired or the email confirmation process has already
-            // been completed
-            abort(404);
+            // Token has expired or the email change process has already been completed
+            abort(410);
         }
 
-        if ($changeRequest->account === null) {
-            throw new \Exception('No account belongs to this email change request');
-        }
-        if (empty($changeRequest->email_new)) {
-            throw new \Exception('Missing `new email` address in email change request');
-        }
-
-        if ($email === $changeRequest->email_previous) {
+        switch ($email) {
+        case $changeRequest->email_previous:
             $changeRequest->is_previous_confirmed = true;
-        } elseif ($email === $changeRequest->email_new) {
+            break;
+
+        case $changeRequest->email_new:
             $changeRequest->is_new_confirmed = true;
-        } else {
-            // if the supplied email matches neither the
-            // old or new email address in the stored
-            // change request, something has gone wrong
-            throw new \Exception('Provided email does not match any email in the stored change request');
+            break;
+
+        default:
+            // If the supplied email matches neither the old nor the new email address in 
+            // the stored email change request, the request cannot be performed
+            throw new \Exception('Provided email address does not match the current or new email address');
         }
 
-        // if the link in both emails has not been
-        // clicked yet, save the current progres and
-        // show the 'confirmation progress' view
-        if ($changeRequest->is_previous_confirmed == false ||
-            $changeRequest->is_new_confirmed == false) {
+        $areBothAddressesVerified = $changeRequest->is_previous_confirmed && $changeRequest->is_new_confirmed;
+
+        if (!$areBothAddressesVerified) {
             $changeRequest->save();
 
             return view('front.pages.account.account-settings-email-confirm', [
                 'changeRequest' => $changeRequest,
             ]);
+
+        } else {
+            // Otherwise, change their email address and complete the process
+            $updateAccountEmail->execute(
+                $changeRequest->account,
+                $changeRequest
+            );
+
+            return view('front.pages.account.account-settings-email-complete');
         }
-
-        // otherwise, change their email address
-        // and complete the process
-        DB::beginTransaction();
-        try {
-            $account = $changeRequest->account;
-
-            // push the email change to Discourse
-            // via the user sync route
-            $payload = (new DiscoursePayload)
-                ->setPcbId($account->getKey())
-                ->setEmail($changeRequest->email_new);
-
-            try {
-                $this->discourseApi->requestSSOSync($payload->build());
-            } catch (\GuzzleHttp\Exception\ServerException $e) {
-                // sometimes the api fails at random because
-                // the 'requires_activation' key is needed in
-                // the payload. As a workaround we'll send the
-                // request again but with the key included
-                // this time
-                $payload->requiresActivation(false);
-                $this->discourseApi->requestSSOSync($payload->build());
-            }
-
-            // push the email change to our own
-            // local user database
-            $account->email = $changeRequest->email_new;
-            $account->save();
-
-            $changeRequest->delete();
-
-            DB::commit();
-
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
-
-        return view('front.pages.account.account-settings-email-complete');
     }
 
     public function changePassword(AccountChangePasswordRequest $request, UpdateAccountPassword $updatePassword)
