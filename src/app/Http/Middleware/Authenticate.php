b9bb9666a782e0d40c3b27d3f8a3660f28e93363
Freddy Heppell <freddy@freddyheppell.com>
2019-07-29 22:46:07 +0100

    Split login and SSO

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -15,6 +15,8 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+
+
             return route('front.login');
         }
     }
