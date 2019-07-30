78a5ae63e39e2e47268b2a0c5d899e00ffa30026
Freddy Heppell <freddy@freddyheppell.com>
2019-07-30 14:34:21 +0100

    Basic testing

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,9 +2,15 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }
