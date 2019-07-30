50719ed1c0ad6fa23c7355e097519315e2e43318
Andy <andy-saw@hotmail.com>
2019-07-30 23:30:29 +0900

    Update deploy controller

--- src/app/Http/Controllers/DeployController.php
+++ src/app/Http/Controllers/DeployController.php
@@ -35,26 +35,31 @@ class DeployController extends WebController
             throw new UnauthorisedException('bad_deploy_key', 'Invalid deployment key specified');
         }
 
-        $this->discord->notifyChannel('Deployment', 'Deployment has begun...');
+        $this->discord->notifyChannel('Deployment', '🕒 Deployment has begun...');
 
         try {
-            $commands = [
-                'cd '.base_path().' php artisan down 2>&1',
-                'cd '.base_path().' git reset --hard 2>&1',
-                'cd '.base_path().' && git checkout ' . $branch . ' 2>&1',
-                'cd '.base_path().' && git pull 2>&1',
-                'cd '.base_path().'/src && php artisan migrate 2>&1',
-                'cd '.base_path().'/src && compose install 2>&1',
-                'cd '.base_path().'/src && npm install 2>&1',
-                'cd '.base_path().'/src && npm run production 2>&1',
-                'cd '.base_path().' php artisan up 2>&1',
-            ];
+            echo shell_exec('cd '.base_path().' php artisan down 2>&1') . '<br>';
 
-            foreach ($commands as $command) {
-                echo shell_exec($command) . '<br>';
-            }
+            $this->discord->notifyChannel('Deployment', '-- Pulling latest version of Master');
+            echo shell_exec('cd '.base_path().' git reset --hard 2>&1') . '<br>';
+            echo shell_exec('cd '.base_path().' && git checkout ' . $branch . ' 2>&1') . '<br>';
+            echo shell_exec('cd '.base_path().' && git pull 2>&1') . '<br>';
+
+            $this->discord->notifyChannel('Deployment', '-- Fetching PHP dependencies');
+            echo shell_exec('cd '.base_path().'/src && compose install 2>&1') . '<br>';
+
+            $this->discord->notifyChannel('Deployment', '-- Fetching JS dependencies');
+            echo shell_exec('cd '.base_path().'/src && npm install 2>&1') . '<br>';
+
+            $this->discord->notifyChannel('Deployment', '-- Building production assets');
+            echo shell_exec('cd '.base_path().'/src && npm run production 2>&1') . '<br>';
+
+            echo shell_exec('chown -R www-data:www-data '.base_path().'/src') . '<br>';
+            echo shell_exec('chmod -R 755 '.base_path().'/src/storage') . '<br>';
+            echo shell_exec('cd '.base_path().' php artisan up 2>&1') . '<br>';
 
             $this->discord->notifyChannel('Deployment', '✔ Deployment complete.');
+
         } catch (\Exception $e) {
             $this->discord->notifyChannel('Deployment', '❌ Deployment failed: '.$e->getMessage());
             Log::fatal($e);
