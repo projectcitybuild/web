405bb028086ae4a64aacf8407a0034236a168d3c
Freddy Heppell <freddy@freddyheppell.com>
2019-08-10 17:54:25 +0100

    Add tests for login

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -5,17 +5,52 @@ namespace Tests\Feature;
 
 
 use App\Entities\Accounts\Models\Account;
+use App\Library\Discourse\Api\DiscourseAdminApi;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
     private $account;
 
-   protected function setUp(): void
-   {
-       parent::setUp();
-       $this->account = factory(Account::class)->create();
-   }
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->account = factory(Account::class)->create();
+    }
+
+    public function testUserIsRedirectedToDiscourseSSOWhenLoggingInWithCorrectCredentials()
+    {
+        $this->post(route('front.login.submit'), [
+            'email' => $this->account->email,
+            'password' => "secret"
+        ])
+            ->assertRedirect("/sso/discourse");
+    }
+
+    public function testUserIsShownErrorWithWrongCredentials()
+    {
+        $this->post(route('front.login.submit'), [
+            'email' => $this->account->email,
+            'password' => "wrong"
+        ]);
+
+        $this->assertFalse(Auth::check());
+    }
+
+    public function testUserWithNullUsernameFetchesFromDiscourse()
+    {
+        $user = factory(Account::class)->create(["username" => null]);
+
+        $this->mock(DiscourseAdminApi::class, function ($mock) use ($user) {
+            $mock->shouldReceive('fetchUserByEmail')->with($user->email)->once();
+        });
+
+        $this->post(route('front.login.submit'), [
+            'email' => $user->email,
+            'password' => "secret"
+        ]);
+    }
 
     public function testRedirectBackToIntendedPage()
     {
@@ -25,9 +60,9 @@ class LoginTest extends TestCase
         $this->withoutExceptionHandling();
 
         $this->post(route('front.login.submit'), [
-                'email' => $this->account->email,
-                'password' => "secret"
-            ])
-           ->assertRedirect(route('front.account.settings'));
+            'email' => $this->account->email,
+            'password' => "secret"
+        ])
+            ->assertRedirect(route('front.account.settings'));
     }
 }
