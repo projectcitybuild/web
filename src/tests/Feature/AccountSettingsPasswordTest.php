7a35292ed1aafb045e0a4f5368a99f6646180828
Freddy Heppell <freddy@freddyheppell.com>
2019-08-13 18:02:52 +0100

    Add password change tests

---
+++ src/tests/Feature/AccountSettingsPasswordTest.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use App\Entities\Accounts\Models\Account;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class AccountSettingsPasswordTest extends TestCase
+{
+    use WithFaker;
+
+    private $account;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->account = factory(Account::class)->create();
+    }
+
+    private function submitPasswordChange($old, $new, $newConfirmation)
+    {
+        return $this->actingAs($this->account)->post(route('front.account.settings.password'), [
+            'old_password' => $old,
+            'new_password' => $new,
+            'new_password_confirm' => $newConfirmation
+        ]);
+    }
+
+    public function testPasswordChange()
+    {
+        $newPassword = $this->faker->password;
+
+        $this->submitPasswordChange("secret", $newPassword, $newPassword)
+            ->assertSessionHasNoErrors();
+
+        $this->assertTrue(Auth::attempt([
+            "email" => $this->account->email,
+            "password" => $newPassword
+        ]));
+    }
+
+    public function testIncorrectCurrentPassword()
+    {
+        $this->submitPasswordChange("wrong", "password", "password")
+            ->assertSessionHasErrors();
+    }
+
+    public function testMismatchingPasswordConfirmation()
+    {
+        $this->submitPasswordChange("secret", "password", "different")
+            ->assertSessionHasErrors();
+    }
+
+    public function testEmptyCurrentPassword()
+    {
+        $this->submitPasswordChange("", "new", "new")
+            ->assertSessionHasErrors();
+    }
+
+    public function testEmptyNewPassword()
+    {
+        $this->submitPasswordChange("secret", "", "")
+            ->assertSessionHasErrors();
+    }
+}
