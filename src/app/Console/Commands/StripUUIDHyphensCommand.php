f7a945ba952e822cf5cc108ad3c582ce22a6559a
andyksaw <andy-saw@hotmail.com>
2019-09-22 00:52:42 +0900

    Wrap strip uuid command in transaction

--- src/app/Console/Commands/StripUUIDHyphensCommand.php
+++ src/app/Console/Commands/StripUUIDHyphensCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
@@ -6,6 +7,7 @@ use App\Entities\Bans\Models\GameBan;
 use App\Entities\Bans\Models\GameUnban;
 use App\Entities\Players\Models\MinecraftPlayer;
 use App\Entities\Players\Models\MinecraftPlayerAlias;
+use Illuminate\Support\Facades\DB;
 
 final class StripUUIDHyphens extends Command
 {
@@ -34,62 +36,64 @@ final class StripUUIDHyphens extends Command
 
         $progressBar = $this->output->createProgressBar(count($playersWithHyphenUuids));
         $progressBar->start();
-        
-        foreach ($playersWithHyphenUuids as $unstrippedPlayer) {
-            // Check if an account exists with the UUID stripped
-            $newUuid = str_replace('-', '', $unstrippedPlayer->uuid);
-            $originalAccount = MinecraftPlayer::where('uuid', $newUuid)->first();
-
-            // If no duplicate account, update the UUID and move on
-            if ($originalAccount === null) {
-                $unstrippedPlayer->uuid = $newUuid;
-                $unstrippedPlayer->save();
-
-                $progressBar->advance();
-                continue;
-            }
-
-            // Otherwise, combine the accounts into the original (stripped) version
-            $bans = GameBan::where('banned_player_id', $unstrippedPlayer->getKey())->get();
-            foreach ($bans as $ban) {
-                $ban->banned_player_id = $originalAccount->getKey();
-                $ban->save();
-            }
-
-            $bans = GameBan::where('staff_player_id', $unstrippedPlayer->getKey())->get();
-            foreach ($bans as $ban) {
-                $ban->staff_player_id = $originalAccount->getKey();
-                $ban->save();
-            }
-
-            $unbans = GameUnban::where('staff_player_id', $unstrippedPlayer->getKey())->get();
-            foreach ($unbans as $unban) {
-                $unban->staff_player_id = $originalAccount->getKey();
-                $unban->save();
-            }
-
-            // Transfer Minecraft alias if necessary
-            $alias = MinecraftPlayerAlias::where('player_minecraft_id', $unstrippedPlayer->getKey())->first();
-            $existingAlias = MinecraftPlayerAlias::where('player_minecraft_id', $originalAccount->getKey())->first();
-            if ($alias !== null && $existingAlias !== null) {
-                if ($existingAlias->updated_at < $alias->updated_at) {
-                    $existingAlias->alias = $alias->alias;
-                    $existingAlias->save();
+     
+        DB::transaction(function() {
+            foreach ($playersWithHyphenUuids as $unstrippedPlayer) {
+                // Check if an account exists with the UUID stripped
+                $newUuid = str_replace('-', '', $unstrippedPlayer->uuid);
+                $originalAccount = MinecraftPlayer::where('uuid', $newUuid)->first();
+    
+                // If no duplicate account, update the UUID and move on
+                if ($originalAccount === null) {
+                    $unstrippedPlayer->uuid = $newUuid;
+                    $unstrippedPlayer->save();
+    
+                    $progressBar->advance();
+                    continue;
                 }
-                $alias->delete();
+    
+                // Otherwise, combine the accounts into the original (stripped) version
+                $bans = GameBan::where('banned_player_id', $unstrippedPlayer->getKey())->get();
+                foreach ($bans as $ban) {
+                    $ban->banned_player_id = $originalAccount->getKey();
+                    $ban->save();
+                }
+    
+                $bans = GameBan::where('staff_player_id', $unstrippedPlayer->getKey())->get();
+                foreach ($bans as $ban) {
+                    $ban->staff_player_id = $originalAccount->getKey();
+                    $ban->save();
+                }
+    
+                $unbans = GameUnban::where('staff_player_id', $unstrippedPlayer->getKey())->get();
+                foreach ($unbans as $unban) {
+                    $unban->staff_player_id = $originalAccount->getKey();
+                    $unban->save();
+                }
+    
+                // Transfer Minecraft alias if necessary
+                $alias = MinecraftPlayerAlias::where('player_minecraft_id', $unstrippedPlayer->getKey())->first();
+                $existingAlias = MinecraftPlayerAlias::where('player_minecraft_id', $originalAccount->getKey())->first();
+                if ($alias !== null && $existingAlias !== null) {
+                    if ($existingAlias->updated_at < $alias->updated_at) {
+                        $existingAlias->alias = $alias->alias;
+                        $existingAlias->save();
+                    }
+                    $alias->delete();
+                }
+    
+                // Transfer linked account_id if necessary
+                if ($unstrippedPlayer->account_id !== null) {
+                    $originalAccount->account_id = $unstrippedPlayer->account_id;
+                    $originalAccount->save();
+                }
+    
+                // Delete unstripped UUID player
+                $unstrippedPlayer->delete();
+    
+                $progressBar->advance();
             }
-
-            // Transfer linked account_id if necessary
-            if ($unstrippedPlayer->account_id !== null) {
-                $originalAccount->account_id = $unstrippedPlayer->account_id;
-                $originalAccount->save();
-            }
-
-            // Delete unstripped UUID player
-            $unstrippedPlayer->delete();
-
-            $progressBar->advance();
-        }
+        });
 
         $progressBar->finish();
     }
