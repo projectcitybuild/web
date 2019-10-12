c5bc59c0479adb6a6519d2daed12157d3245e438
andyksaw <andy-saw@hotmail.com>
2019-10-13 01:22:31 +0900

    Make Group factory

--- src/tests/Unit/App/Console/DeactivateDonatorPerksCommand_Test.php
+++ src/tests/Unit/App/Console/DeactivateDonatorPerksCommand_Test.php
@@ -5,12 +5,30 @@ namespace Tests;
 use App\Console\Commands\DeactivateDonatorPerksCommand;
 use App\Entities\Accounts\Models\Account;
 use App\Entities\Donations\Models\DonationPerk;
+use App\Entities\Groups\Models\Group;
+use App\Http\Actions\SyncUserToDiscourse;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 final class DeactivateDonatorPerksCommand_Test extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        factory(Group::class)->create(['name' => 'donator']);
+    }
+
+    private function makeCommand() : DeactivateDonatorPerksCommand
+    {
+        $mockSyncAction = $this->getMockBuilder(SyncUserToDiscourse::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return new DeactivateDonatorPerksCommand($mockSyncAction);
+    }
     
     public function testDeactivatesExpiredPerk()
     {
@@ -21,7 +39,7 @@ final class DeactivateDonatorPerksCommand_Test extends TestCase
             'account_id' => factory(Account::class)->create()->getKey(),
         ]);
 
-        $command = resolve(DeactivateDonatorPerksCommand::class);
+        $command = $this->makeCommand();
         $command->handle();
 
         $perk = DonationPerk::find($expectedPerk->getKey());
@@ -38,7 +56,7 @@ final class DeactivateDonatorPerksCommand_Test extends TestCase
             'account_id' => factory(Account::class)->create()->getKey(),
         ]);
 
-        $command = resolve(DeactivateDonatorPerksCommand::class);
+        $command = $this->makeCommand();
         $command->handle();
 
         $perk = DonationPerk::find($expectedPerk->getKey());
