3c9d773f057c9717800b64284388ea9cadfea4b3
andyksaw <andy-saw@hotmail.com>
2019-10-13 00:37:53 +0900

    Fix broken factory

--- src/database/factories/Donations/DonationFactory.php
+++ src/database/factories/Donations/DonationFactory.php
@@ -9,9 +9,6 @@ use App\Entities\Donations\Models\Donation;
 $factory->define(Donation::class, function (Faker\Generator $faker) {
     return [
         'amount' => $faker->numberBetween(3, 100),
-        'is_active' => true,
-        'is_lifetime_perks' => true,
-        'perks_end_at' => $faker->dateTime(),
         'created_at' => $faker->dateTime(),
         'updated_at' => $faker->dateTime(),
     ];
