f46907a3b40262c619519bb39830e5762996fd49
andyksaw <andy-saw@hotmail.com>
2020-02-18 23:05:53 +0900

    Grab subscription id

--- src/app/Library/Stripe/StripePaymentWebhook.php
+++ src/app/Library/Stripe/StripePaymentWebhook.php
@@ -6,13 +6,15 @@ final class StripePaymentWebhook extends StripeWebhook
 {
     private $sessionId;
     private $amountInCents;
+    private $subscriptionId;
 
-    public function __construct(StripeWebhookEvent $event, string $transactionId, string $sessionId, int $amountInCents)
+    public function __construct(StripeWebhookEvent $event, string $transactionId, string $sessionId, int $amountInCents, ?string $subscriptionId)
     {
         parent::_construct($event, $transactionId);
 
         $this->sessionId = $sessionId;
         $this->amountInCents = $amountInCents;
+        $this->subscriptionId = $subscriptionId;
     }
 
     public static function fromJSON($json, StripeWebhookEvent $event): ?StripeWebhook
@@ -20,11 +22,17 @@ final class StripePaymentWebhook extends StripeWebhook
         $object = $json->data->object;
         $displayItem = $object->display_items[0];
 
+        $subscriptionId = null;
+        if ($object->subscription) {
+            $subscriptionId = $object->subscription;
+        }
+
         return new StripePaymentWebhook(
             $event,
             $object->id,
             $object->client_reference_id,
-            $displayItem->amount * $displayItem->quantity
+            $displayItem->amount * $displayItem->quantity,
+            $subscriptionId
         );
     }
 
