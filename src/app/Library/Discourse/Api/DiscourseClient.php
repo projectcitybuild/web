90eb00797c3fcaa8a2071a5181d3db01be22b275
Freddy Heppell <freddy@freddyheppell.com>
2020-05-10 12:26:32 +0100

    Switch to header-based discourse auth

--- src/app/Library/Discourse/Api/DiscourseClient.php
+++ src/app/Library/Discourse/Api/DiscourseClient.php
@@ -6,10 +6,26 @@ use GuzzleHttp\Client;
 
 final class DiscourseClient extends Client
 {
+    private function getApiKey()
+    {
+        return config('discourse.api_key');
+    }
+
+    private function getApiUser()
+    {
+        return config('discourse.api_user');
+    }
+
     public function __construct()
     {
         parent::__construct([
             'base_uri' => 'https://forums.projectcitybuild.com/',
+            [
+                'headers' => [
+                    'Api-Key' => $this->getApiKey(),
+                    'Api-User' => $this->getApiUser()
+                ]
+            ]
         ]);
     }
 }
