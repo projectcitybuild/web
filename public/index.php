a90c0dfe9db763a296353d77880b8d6e631a9b3b
Freddy Heppell <freddy@freddyheppell.com>
2020-12-19 17:35:24 +0000

    Remove src directory

--- public/index.php
+++ public/index.php
@@ -35,7 +35,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
