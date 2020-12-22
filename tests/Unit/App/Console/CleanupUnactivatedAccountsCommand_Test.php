d333170bbe6e0ab9a9be90f048dc96494e73de40
andyksaw <andy-saw@hotmail.com>
2020-12-23 00:11:12 +0900

    Update test

--- tests/Unit/App/Console/CleanupUnactivatedAccountsCommand_Test.php
+++ tests/Unit/App/Console/CleanupUnactivatedAccountsCommand_Test.php
@@ -2,45 +2,39 @@
 
 namespace Tests;
 
-use App\Console\Commands\CleanupUnactivatedAccountsCommand;
 use App\Entities\Accounts\Models\Account;
-use App\Entities\Donations\Models\DonationPerk;
-use App\Entities\Groups\Models\Group;
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 final class CleanupUnactivatedAccountsCommand_Test extends TestCase
 {
     use RefreshDatabase;
 
-    public function testCascadeDeletesUnactivatedAccount()
+    public function testDeletesUnactivatedAccount()
     {
         $elapsedDaysToDelete = 14;
 
-        $associatedGroup = factory(Group::class)->states('as-default')->create();
-
-        $unactivatedAccount = factory(Account::class)->states('unactivated')->create();
-        $unactivatedAccount->groups()->attach($unactivatedAccount->getKey());
-        $unactivatedAccount->updated_at = now()->subDays($elapsedDaysToDelete + 1);
-        $unactivatedAccount->save();
+        $accountToBeDeleted = factory(Account::class)->states('unactivated')->create([
+            'updated_at' => now()->subDays($elapsedDaysToDelete + 1),
+        ]);
+        $accountJustCreated = factory(Account::class)->states('unactivated')->create([
+            'updated_at' => now(),
+        ]);
 
         $this->assertDatabaseHas('accounts', [
-            'account_id' => $unactivatedAccount->getKey(),
+            'account_id' => $accountToBeDeleted->getKey(),
         ]);
-        $this->assertDatabaseHas('groups_accounts', [
-            'account_id' => $unactivatedAccount->getKey(),
-            'group_id' => $associatedGroup->getKey(),
+        $this->assertDatabaseHas('accounts', [
+            'account_id' => $accountJustCreated->getKey(),
         ]);
 
         $this->artisan('cleanup:unactivated-accounts --days='.$elapsedDaysToDelete)
             ->assertExitCode(0);
 
         $this->assertDatabaseMissing('accounts', [
-            'account_id' => $unactivatedAccount->getKey(),
+            'account_id' => $accountToBeDeleted->getKey(),
         ]);
-        $this->assertDatabaseMissing('groups_accounts', [
-            'account_id' => $unactivatedAccount->getKey(),
-            'group_id' => $associatedGroup->getKey(),
+        $this->assertDatabaseHas('accounts', [
+            'account_id' => $accountJustCreated->getKey(),
         ]);
     }
 }
