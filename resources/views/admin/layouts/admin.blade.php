06ae19bf75ccd853bfade535f8c638033f350069
Freddy Heppell <freddy@freddyheppell.com>
2021-06-15 10:22:17 +0100

    Minor UI improvements

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Dashboard Template · Bootstrap v5.0</title>
+    <title>Staff Panel - @yield('title')</title>
 
 
     <!-- Bootstrap core CSS -->
