d006f2a77ae2318f9c0a9854ac117a2fb0047c94
Freddy Heppell <freddy@freddyheppell.com>
2021-06-17 18:06:51 +0100

    Remove donation perk index

--- routes/web.php
+++ routes/web.php
@@ -247,7 +247,7 @@ Route::group(['prefix' => 'panel', 'as' => 'front.panel.', 'namespace' => 'Panel
 
     Route::resource('accounts', 'AccountController')->only(['index', 'show', 'edit', 'update']);
     Route::resource('donations', 'DonationController');
-    Route::resource('donation-perks', 'DonationPerksController')->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+    Route::resource('donation-perks', 'DonationPerksController')->only(['create', 'store', 'edit', 'update', 'destroy']);
 
     Route::group(['prefix' => 'accounts/{account}', 'as' => 'accounts.'], function () {
         Route::get('discourse-admin', [
