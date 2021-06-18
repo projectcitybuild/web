3429f2f50c649e27b97e8b1cee7ea4925ffa436d
Freddy Heppell <freddy@freddyheppell.com>
2021-06-18 14:03:53 +0100

    Fix admin JS splitting

--- resources/js/admin/admin.ts
+++ resources/js/admin/admin.ts
@@ -1,10 +1,7 @@
 import {Tooltip} from 'bootstrap';
 
-console.log('hey');
 const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-console.log(tooltipTriggerList);
 tooltipTriggerList.map(function (tooltipTriggerEl: HTMLElement): Tooltip {
-    console.log(tooltipTriggerEl);
     return new Tooltip(tooltipTriggerEl)
 })
 
