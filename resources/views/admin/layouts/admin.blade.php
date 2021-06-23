c20059d389ca60cb2e41bb7513b685267ebc4594
Freddy Heppell <freddy@freddyheppell.com>
2021-06-23 11:49:59 +0100

    Remove unused template UI components

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -28,12 +28,12 @@
                 aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
+{{--        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">--}}
+{{--        <ul class="navbar-nav px-3">--}}
+{{--            <li class="nav-item text-nowrap">--}}
+{{--                <a class="nav-link" href="#">Sign out</a>--}}
+{{--            </li>--}}
+{{--        </ul>--}}
     </header>
 
     <div class="container-fluid">
