d5e04271734226137cdcc13615b13624bba075cb
Freddy Heppell <freddyheppell@users.noreply.github.com>
2021-07-04 14:10:23 +0000

    Apply fixes from StyleCI

--- app/Entities/Players/Models/MinecraftPlayer.php
+++ app/Entities/Players/Models/MinecraftPlayer.php
@@ -50,7 +50,8 @@ final class MinecraftPlayer extends Model implements BannableModelInterface
     public function getDashedUuidAttribute()
     {
         $uuid = $this->uuid;
-        return substr($uuid, 0, 8) . '-' . substr($uuid, 8, 4) . '-' . substr($uuid, 12, 4) . '-' . substr($uuid, 16, 4) . '-' . substr($uuid, 20);
+
+        return substr($uuid, 0, 8).'-'.substr($uuid, 8, 4).'-'.substr($uuid, 12, 4).'-'.substr($uuid, 16, 4).'-'.substr($uuid, 20);
     }
 
     public function account(): BelongsTo
