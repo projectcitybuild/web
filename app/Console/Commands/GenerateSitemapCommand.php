402f3c08e6f651fd6141d1bc1540b86d0da9f154
andyksaw <andy-saw@hotmail.com>
2021-07-09 21:03:21 +0900

    Add command to generate sitemap

---
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Spatie\Sitemap\SitemapGenerator;
+
+final class GenerateSitemapCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates a sitemap.xml file to help aid search crawlers';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(config('app.url'))
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
