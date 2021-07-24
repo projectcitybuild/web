bf15fab7051c5040bd1f27d90a98fa0cfd7181d5
Freddy Heppell <freddy@freddyheppell.com>
2021-07-24 20:40:10 +0100

    Redesign 2fa setup

--- resources/views/v2/front/components/form-error.blade.php
+++ resources/views/v2/front/components/form-error.blade.php
@@ -1,6 +1,6 @@
 @if($errors->any())
     <div class="alert alert--error">
-        <h3><i class="fas fa-exclamation-circle"></i> Error</h3>
+        <h2><i class="fas fa-exclamation-circle"></i> Error</h2>
         @foreach($errors->all() as $error)
             <div>{{ $error }}</div>
         @endforeach
