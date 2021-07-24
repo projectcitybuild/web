30ff3dacb1dc842947b919976e5d28ca23b379ee
Freddy Heppell <freddy@freddyheppell.com>
2021-07-24 17:39:57 +0100

    Fix settings alert width

--- resources/views/v2/front/pages/account/settings/_email.blade.php
+++ resources/views/v2/front/pages/account/settings/_email.blade.php
@@ -1,21 +1,21 @@
+@if(Session::has('success'))
+    <div class="alert alert--success">
+        <h2><i class="fas fa-exclamation-circle"></i> Success</h2>
+        {{ Session::get('success') }}
+    </div>
+    <p>
+@endif
+@if($errors->email->any())
+    <div class="alert alert--error">
+        <h2><i class="fas fa-exclamation-circle"></i> Error</h2>
+        @foreach($errors->email->all() as $error)
+            {{ $error }}<br>
+        @endforeach
+    </div>
+@endif
+
 <form method="post" action="{{ route('front.account.settings.email') }}" class="settings__form">
     @csrf
-
-    @if(Session::has('success'))
-        <div class="alert alert--success">
-            <h3><i class="fas fa-exclamation-circle"></i> Success</h3>
-            {{ Session::get('success') }}
-        </div>
-        <p>
-    @endif
-    @if($errors->email->any())
-        <div class="alert alert--error">
-            <h2><i class="fas fa-exclamation-circle"></i> Error</h2>
-            @foreach($errors->email->all() as $error)
-                {{ $error }}<br>
-            @endforeach
-        </div>
-    @endif
 
     <label for="email">Email</label>
     <input class="textfield {{ $errors->has('email') ? 'error' : '' }}" name="email" type="email" id="email"
