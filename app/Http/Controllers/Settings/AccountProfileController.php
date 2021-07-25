e8a1b84e0628cf39b7af133140a787756e3bbb76
Freddy Heppell <freddy@freddyheppell.com>
2021-07-25 22:07:57 +0100

    Apply fixes from StyleCI (#449)
    Co-authored-by: Freddy Heppell <freddyheppell@users.noreply.github.com>

--- app/Http/Controllers/Settings/AccountProfileController.php
+++ app/Http/Controllers/Settings/AccountProfileController.php
@@ -10,6 +10,7 @@ class AccountProfileController extends WebController
     public function show(Request $request)
     {
         $account = $request->user();
+
         return view('v2.front.pages.account.profile')->with(compact('account'));
     }
 }
