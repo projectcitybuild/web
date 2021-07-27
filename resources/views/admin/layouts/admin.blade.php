b8f369d6e84e5078809729681ad84c3d60a15171
Freddy Heppell <freddy@freddyheppell.com>
2021-07-27 19:50:26 +0100

    Improve nav colours

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -20,7 +20,7 @@
 <body>
 <div id="app">
 
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('front.panel.index') }}">
             <img src="/assets/images/logo.png" alt="Project City Build" height="30">
         </a>
