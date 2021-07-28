7d7c977e7733e194d062981552688c127d463cd6
Freddy Heppell <freddy@freddyheppell.com>
2021-07-28 13:37:21 +0100

    Change menu item name

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -97,7 +97,7 @@
                 <ul>
                     <li><a href="https://forums.projectcitybuild.com/t/banned-read-me/12145">Appeal a Ban</a></li>
                     <li><a href="https://forums.projectcitybuild.com/c/support/grief-player-reports">Report a Player</a></li>
-                    <li><a href="{{ route('rankup') }}">Apply for Architect</a></li>
+                    <li><a href="{{ route('rankup') }}">Apply for Building Rank</a></li>
                     <li class="divider"><a href="https://goo.gl/forms/UodUsKQBZJdCzNWk1">Apply for Staff</a></li>
                     <li class="divider"><a href="{{ route('front.donate') }}">Donate</a></li>
                     <li><a href="https://wiki.projectcitybuild.com/" target="_blank" rel="noopener">Community Wiki</a>
