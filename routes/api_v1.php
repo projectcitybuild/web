d174f3701dc86ab52645ba969728665a95c66dfe
andyksaw <andy-saw@hotmail.com>
2022-04-13 17:14:48 +0900

    Allow IDE jumping for API routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\v1\GameBanController;
+use App\Http\Controllers\Api\v1\GroupApiController;
+use App\Http\Controllers\Api\v1\MinecraftAuthTokenController;
 use App\Http\Controllers\Api\v1\MinecraftBalanceController;
+use App\Http\Controllers\Api\v1\MinecraftDonationTierController;
+use App\Http\Controllers\Api\v1\StripeWebhookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,27 +20,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('webhooks')->group(function () {
-    Route::post('stripe', 'StripeWebhookController@handleWebhook')->name('cashier.webhook');
+    Route::post('stripe', [StripeWebhookController::class, 'handleWebhook'])->name('cashier.webhook');
 });
 
 Route::prefix('bans')->group(function () {
-    Route::post('list', 'GameBanController@getBanList');
-    Route::post('store/ban', 'GameBanController@storeBan');
-    Route::post('store/unban', 'GameBanController@storeUnban');
-    Route::post('status', 'GameBanController@getPlayerStatus');
+    Route::post('list', [GameBanController::class, 'getBanList']);
+    Route::post('store/ban', [GameBanController::class, 'storeBan']);
+    Route::post('store/unban', [GameBanController::class, 'storeUnban']);
+    Route::post('status', [GameBanController::class, 'getPlayerStatus']);
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('minecraft', 'MinecraftAuthTokenController@store');
-    Route::get('minecraft/{minecraftUUID}', 'MinecraftAuthTokenController@show');
+    Route::post('minecraft', [MinecraftAuthTokenController::class, 'store']);
+    Route::get('minecraft/{minecraftUUID}', [MinecraftAuthTokenController::class, 'show']);
 });
 
 Route::prefix('minecraft/{minecraftUUID}')->group(function () {
-    Route::get('donation-tiers', 'MinecraftDonationTierController@show');
+    Route::get('donation-tiers', [MinecraftDonationTierController::class, 'show']);
     Route::get('balance', [MinecraftBalanceController::class, 'show']);
     Route::post('balance/deduct', [MinecraftBalanceController::class, 'deduct']);
 });
 
 Route::prefix('groups')->group(function () {
-    Route::get('/', 'GroupApiController@getAll');
+    Route::get('/', [GroupApiController::class, 'getAll']);
 });
