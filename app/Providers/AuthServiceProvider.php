94f4f4f253e5574db62be6c204650a49913eb87c
Freddy Heppell <freddy@freddyheppell.com>
2022-04-21 12:36:31 +0100

    Add 2FA bypass (#535)
    * Add 2FA bypass
    
    * Correct nitpicks
    
    * Try disabling phpdbg
    
    * Mock config for all tests

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Library\Google2FA\Google2FAFake;
+use PragmaRX\Google2FA\Google2FA;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,5 +25,17 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+    }
+
+    /**
+     * Register any auth-related application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (config('auth.totp.bypass')) {
+            $this->app->bind(abstract: Google2FA::class, concrete: Google2FAFake::class);
+        }
     }
 }
