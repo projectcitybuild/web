7780f59088e0c0e7a68d000051e68470109c0d4e
Freddy Heppell <freddy@freddyheppell.com>
2022-04-21 12:16:46 +0100

    Mock config for all tests

--- tests/Unit/Library/Google2FA/Google2FAFakeTest.php
+++ tests/Unit/Library/Google2FA/Google2FAFakeTest.php
@@ -9,25 +9,57 @@ use Tests\TestCase;
 
 class Google2FAFakeTest extends TestCase
 {
-    public function testVerifyKeyNewerFailsIfNotDebug()
+    private function mockAuthBypassConf($toBe)
+    {
+        Config::shouldReceive('get')
+            ->with('auth.totp.bypass', null)
+            ->andReturn($toBe);
+
+        return $this;
+    }
+
+    private function mockAppDebug($toBe)
     {
         Config::shouldReceive('get')
             ->with('app.debug', null)
-            ->andReturn(false);
+            ->andReturn($toBe);
+
+        return $this;
+    }
+
+    private function mockCorrectBypassEnv()
+    {
+        $this->mockAppDebug(true)->mockAuthBypassConf(true);
+    }
+
+    public function testVerifyKeyNewerFailsIfNotDebug()
+    {
+        $this->mockAppDebug(false)->mockAuthBypassConf(true);
         $google2FA = new Google2FAFake();
 
         $this->expectException(Dangerous2FABypassException::class);
         $google2FA->verifyKeyNewer('secret', 'key', 1234);
     }
 
+    public function testVerifyKeyNewerFailsIfBypassNotEnabled()
+    {
+        $this->mockAppDebug(true)->mockAuthBypassConf(false);
+
+        $google2FA = new Google2FAFake();
+        $this->expectException(Dangerous2FABypassException::class);
+        $google2FA->verifyKeyNewer('secret', 'key', 1234);
+    }
+
     public function testVerifyKeyNewerSucceedsWithBypassCode()
     {
+        $this->mockCorrectBypassEnv();
         $google2FA = new Google2FAFake();
         $this->assertIsInt($google2FA->verifyKeyNewer('secret', '0000', 1234));
     }
 
     public function testVerifyNewerKeyFailsWithOtherCode()
     {
+        $this->mockCorrectBypassEnv();
         $google2FA = new Google2FAFake();
         $this->assertFalse($google2FA->verifyKeyNewer('secret', '5678', 1234));
     }
