8b0bbdf1cec0691c6ae55ba34a27b02bfda58047
Freddy Heppell <freddy@freddyheppell.com>
2022-05-24 19:02:39 +0100

    Fix update email use case test

--- tests/Unit/Domain/EmailChange/UseCases/UpdateAccountEmailUseCaseTest.php
+++ tests/Unit/Domain/EmailChange/UseCases/UpdateAccountEmailUseCaseTest.php
@@ -2,38 +2,23 @@
 
 namespace Unit\Domain\EmailChange\UseCases;
 
-use Domain\EmailChange\Notifications\VerifyNewEmailAddressNotification;
-use Domain\EmailChange\Notifications\VerifyOldEmailAddressNotification;
-use Domain\EmailChange\UseCases\SendVerificationEmailUseCase;
 use Domain\EmailChange\UseCases\UpdateAccountEmailUseCase;
 use Entities\Models\Eloquent\Account;
 use Entities\Models\Eloquent\AccountEmailChange;
-use Entities\Repositories\AccountEmailChangeRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Notification;
-use Library\SignedURL\Adapters\StubSignedURLGenerator;
-use Library\SignedURL\SignedURLGenerator;
-use Library\Tokens\Adapters\StubTokenGenerator;
-use Library\Tokens\TokenGenerator;
-use Shared\ExternalAccounts\Sync\ExternalAccountSync;
 use Tests\TestCase;
 
 class UpdateAccountEmailUseCaseTest extends TestCase
 {
     use RefreshDatabase;
 
-    private ExternalAccountSync $externalAccountSync;
     private UpdateAccountEmailUseCase $useCase;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->externalAccountSync = \Mockery::mock(ExternalAccountSync::class);
-
-        $this->useCase = new UpdateAccountEmailUseCase(
-            externalAccountSync: $this->externalAccountSync
-        );
+        $this->useCase = new UpdateAccountEmailUseCase();
     }
 
     public function test_updates_email()
@@ -44,10 +29,6 @@ class UpdateAccountEmailUseCaseTest extends TestCase
         $changeRequest = AccountEmailChange::factory()
             ->for($account)
             ->create(['email_new' => $newEmail]);
-
-        $this->externalAccountSync
-            ->shouldReceive('sync')
-            ->with($account);
 
         $this->assertEquals(expected: $oldEmail, actual: $account->email);
         $this->assertDatabaseHas(
