bf468799c41316969803a99ea7adb9fd2787bf8f
Freddy Heppell <freddy@freddyheppell.com>
2022-06-03 00:27:08 +0100

    Enable recaptcha validation

--- app/Http/Requests/BanLookupRequest.php
+++ app/Http/Requests/BanLookupRequest.php
@@ -25,7 +25,8 @@ class BanLookupRequest extends FormRequest
     public function rules()
     {
         return [
-            'username' => 'required|min:3|max:16|alpha_dash'
+            'username' => 'required|min:3|max:16|alpha_dash',
+            'g-recaptcha-response' => 'recaptcha',
         ];
     }
 }
