5b3c73a626bf919cbcdd65629eadf651ac47b346
Freddy Heppell <freddy@freddyheppell.com>
2022-06-03 14:12:22 +0100

    Swap debugbar to telescope (#578)
    * Remove debugbar
    
    * Install telescope
    
    * Set up telescope access
    
    * Add telescope toolbar
    
    * Add telescope prune to schedule
    
    * Fix telescope environment check
    
    * Fix additional environment check
    
    * Restrict telescope access in production
    
    * Publish assets for CI
    
    * Run telescope prune in dev
    
    * Add composer hooks

---
+++ tests/Feature/TelescopeAccessTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Feature;
+
+use Illuminate\Support\Facades\Config;
+use Tests\TestCase;
+
+class TelescopeAccessTest extends TestCase
+{
+    protected string $originalEnv;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->originalEnv = config('app.env');
+        Config::set('app.env', 'production');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        Config::set('app.env', $this->originalEnv);
+    }
+
+
+    public function test_telescope_access_forbidden()
+    {
+        $this->get('/telescope')
+            ->assertForbidden();
+    }
+
+    public function test_telescope_admin_allowed()
+    {
+        $this->actingAs($this->adminAccount())
+            ->get('/telescope')
+            ->assertOk();
+    }
+}
