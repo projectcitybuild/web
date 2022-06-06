10e972fc59f90b1478f36424e23996b4d98a8d76
andyksaw <andy-saw@hotmail.com>
2022-06-06 22:51:15 +0900

    Fix PanelTest

--- tests/Feature/PanelTest.php
+++ tests/Feature/PanelTest.php
@@ -4,10 +4,23 @@ namespace Tests\Feature;
 
 use Entities\Models\Eloquent\Account;
 use Entities\Models\Eloquent\Group;
+use Entities\Models\Eloquent\GroupScope;
+use Entities\Models\PanelGroupScope;
 use Tests\TestCase;
 
 class PanelTest extends TestCase
 {
+    private function createPanelAccessGroup(): Group
+    {
+        $group = Group::factory()->administrator()->create();
+        $group->groupScopes()->attach(
+            collect([PanelGroupScope::ACCESS_PANEL])
+                ->map(fn($case) => GroupScope::factory()->create(['scope' => $case->value]))
+                ->map(fn($model) => $model->getKey())
+        );
+        return $group;
+    }
+
     public function test_guest_cannot_see_panel()
     {
         $this->get(route('front.panel.index'))
@@ -16,11 +29,12 @@ class PanelTest extends TestCase
 
     public function test_non_mfa_user_cannot_see_panel()
     {
-        $nonMfaAdmin = Account::factory()
-            ->has(Group::factory()->administrator())
-            ->create();
+        $nonMfaAccount = Account::factory()->create();
+        $nonMfaAccount->groups()->attach(
+            $this->createPanelAccessGroup()
+        );
 
-        $this->actingAs($nonMfaAdmin)
+        $this->actingAs($nonMfaAccount)
             ->get(route('front.panel.index'))
             ->assertRedirect(route('front.account.security'))
             ->assertSessionHas('mfa_setup_required', true);
@@ -30,7 +44,12 @@ class PanelTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $this->actingAs($this->adminAccount())
+        $mfaAccount = Account::factory()->hasFinishedTotp()->create();
+        $mfaAccount->groups()->attach(
+            $this->createPanelAccessGroup()
+        );
+
+        $this->actingAs($mfaAccount)
             ->get(route('front.panel.index'))
             ->assertOk();
     }
