f5deb4fb4d3444e5fa5ec7ca536969fecdd97fa4
andyksaw <andy-saw@hotmail.com>
2022-06-10 01:12:36 +0900

    Replace selectize with choices.js

--- resources/js/admin/user-picker.ts
+++ resources/js/admin/user-picker.ts
@@ -1,31 +1,36 @@
-import $ from 'jquery';
-import 'selectize/dist/js/selectize';
+// import $ from 'jquery';
+// import 'selectize/dist/js/selectize';
 
-const userSelectElements = [].slice.call(document.querySelectorAll('[data-pcb-user-picker]'));
+// const userSelectElements = [].slice.call(document.querySelectorAll('[data-pcb-user-picker]'));
+//
+// userSelectElements.map(function (userSelectEl: HTMLElement) {
+//     console.info("Initialising user select " + userSelectEl);
+//     $(userSelectEl).selectize({
+//         valueField: "account_id",
+//         labelField: "username",
+//         searchField: ["username", "email", "id"],
+//         create: false,
+//         closeAfterSelect: true,
+//         placeholder: "Start typing...",
+//         // openOnFocus: true,
+//         render: {
+//             option: function (item, escape) {
+//                 return (
+//                     `<div class="option">#${escape(item.account_id)}: ${escape(item.username)} <span class="text-muted">(${escape(item.email)})</span></div>`
+//                 );
+//             },
+//         },
+//         load: function (query: string, callback: Function) {
+//             if (!query.length) return callback();
+//             $.get('/panel/api/accounts?query=' + query)
+//                 .then((res) => {
+//                     callback(res.data);
+//                 })
+//         },
+//     });
+// });
 
-userSelectElements.map(function (userSelectEl: HTMLElement) {
-    console.info("Initialising user select " + userSelectEl);
-    $(userSelectEl).selectize({
-        valueField: "account_id",
-        labelField: "username",
-        searchField: ["username", "email", "id"],
-        create: false,
-        closeAfterSelect: true,
-        placeholder: "Start typing...",
-        // openOnFocus: true,
-        render: {
-            option: function (item, escape) {
-                return (
-                    `<div class="option">#${escape(item.account_id)}: ${escape(item.username)} <span class="text-muted">(${escape(item.email)})</span></div>`
-                );
-            },
-        },
-        load: function (query: string, callback: Function) {
-            if (!query.length) return callback();
-            $.get('/panel/api/accounts?query=' + query)
-                .then((res) => {
-                    callback(res.data);
-                })
-        },
-    });
-});
+import Choices from 'choices.js';
+
+const element = document.querySelector('[data-pcb-user-picker]');
+const choices = new Choices(element);
