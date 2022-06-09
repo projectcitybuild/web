e2d33d8e28cdbf6aadc754bb8c800f331b1d7a97
andyksaw <andy-saw@hotmail.com>
2022-06-10 02:26:59 +0900

    Fetch accounts from API

--- resources/js/admin/user-picker.ts
+++ resources/js/admin/user-picker.ts
@@ -33,4 +33,36 @@
 import Choices from 'choices.js';
 
 const element = document.querySelector('[data-pcb-user-picker]');
-const choices = new Choices(element);
+const choices = new Choices(element, {
+    allowHTML: false,
+    callbackOnInit: () => {
+        let task: number | null = null
+
+        element.addEventListener(
+            'search',
+            (event) => {
+                // @ts-expect-error
+                const query = event.detail.value;
+
+                clearTimeout(task);
+                task = window.setTimeout(() => {
+                    choices.setChoices(async () => {
+                        try {
+                            // TODO: grab URL from .env file since
+                            const accounts = await fetch('../api/accounts?query=' + query);
+                            const json = await accounts.json()
+
+                            // @ts-expect-error
+                            return json.data.map((account) => {
+                                return { value: account.account_id, label: `${account.username} (${account.email})` }
+                            });
+                        } catch (err) {
+                            console.log(err);
+                        }
+                    });
+                }, 350);
+            },
+            false,
+        );
+    }
+});
