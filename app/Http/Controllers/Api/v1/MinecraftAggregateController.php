00b293c28ec6d9b16fda7fd97fbd807e7723175c
Andy Saw <andy-saw@hotmail.com>
2022-08-09 02:58:32 +0900

    Add linked account to response

--- app/Http/Controllers/Api/v1/MinecraftAggregateController.php
+++ app/Http/Controllers/Api/v1/MinecraftAggregateController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Exceptions\Http\BadRequestException;
 use App\Exceptions\Http\NotFoundException;
+use App\Exceptions\Http\UnauthorisedException;
 use App\Http\ApiController;
 use Domain\Badges\UseCases\GetBadgesUseCase;
 use Domain\Bans\UseCases\GetBanUseCase;
 use Domain\Donations\UseCases\GetDonationTiersUseCase;
+use Entities\Models\Eloquent\Account;
+use Entities\Models\Eloquent\MinecraftPlayer;
 use Entities\Models\PlayerIdentifierType;
+use Entities\Resources\AccountResource;
 use Entities\Resources\DonationPerkResource;
 use Entities\Resources\GameBanResource;
 use Illuminate\Http\Request;
@@ -33,12 +38,30 @@ final class MinecraftAggregateController extends ApiController
             $donationTiers = [];
         }
 
+        $account = $this->getLinkedAccount($uuid);
+
         return [
             'data' => [
+                'account' => is_null($account) ? null : new AccountResource($account),
                 'ban' => is_null($ban) ? null : new GameBanResource($ban),
                 'badges' => $badges,
                 'donation_tiers' => DonationPerkResource::collection($donationTiers),
             ],
         ];
+    }
+
+    // TODO: share this logic with MinecraftAuthTokenController
+    private function getLinkedAccount(string $uuid): ?Account
+    {
+        $existingPlayer = MinecraftPlayer::where('uuid', $uuid)->first();
+
+        if ($existingPlayer === null || $existingPlayer->account === null) {
+            return null;
+        }
+        // Force load groups
+        $existingPlayer->account->groups;
+        $existingPlayer->touchLastSyncedAt();
+
+        return $existingPlayer->account;
     }
 }
