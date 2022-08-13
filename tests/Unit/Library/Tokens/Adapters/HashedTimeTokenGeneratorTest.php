60343070834b78531762da8cabd349e5344dafc3
Freddy Heppell <freddy@freddyheppell.com>
2022-08-13 12:11:02 +0100

    Change other tests to use new temp config

--- tests/Unit/Library/Tokens/Adapters/HashedTimeTokenGeneratorTest.php
+++ tests/Unit/Library/Tokens/Adapters/HashedTimeTokenGeneratorTest.php
@@ -3,16 +3,19 @@
 namespace Tests\Unit\Library\Tokens\Adapters;
 
 use Library\Tokens\Adapters\HashedTokenGenerator;
+use Tests\Support\TemporaryConfig;
 use Tests\TestCase;
 
 final class HashedTimeTokenGeneratorTest extends TestCase
 {
+    use TemporaryConfig;
+
     public function test_generates_token()
     {
         $key = 'key';
         $bytes = 'test';
 
-        config()->set('app.key', $key);
+        $this->setTemporaryConfig('app.key', $key);
 
         $tokenGenerator = new HashedTokenGenerator(
             byteGenerator: fn () => $bytes,
