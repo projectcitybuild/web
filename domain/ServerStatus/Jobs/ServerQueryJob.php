a1312772d401d3436beef7c1884c6b774dce269c
Andy Saw <andy-saw@hotmail.com>
2022-08-22 19:55:19 +0900

    Prevent querying the same server too many times

--- domain/ServerStatus/Jobs/ServerQueryJob.php
+++ domain/ServerStatus/Jobs/ServerQueryJob.php
@@ -9,11 +9,15 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 
 final class ServerQueryJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     /**
      * Create a new job instance.
@@ -21,6 +25,14 @@ final class ServerQueryJob implements ShouldQueue
     public function __construct(
         private Server $server
     ) {
+    }
+
+    /**
+     * Get the middleware the job should pass through.
+     */
+    public function middleware(): array
+    {
+        return [new WithoutOverlapping($this->server->getKey())];
     }
 
     /**
