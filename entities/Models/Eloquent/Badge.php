a8787f1c1c8116405a87af27538b2d1ad7d56384
Freddy Heppell <freddy@freddyheppell.com>
2022-08-27 22:24:37 +0100

    Make badge final to appease larastan

--- entities/Models/Eloquent/Badge.php
+++ entities/Models/Eloquent/Badge.php
@@ -9,7 +9,7 @@ use Library\Auditing\AuditAttributes;
 use Library\Auditing\Concerns\LogsActivity;
 use Library\Auditing\Contracts\LinkableAuditModel;
 
-class Badge extends Model implements LinkableAuditModel
+final class Badge extends Model implements LinkableAuditModel
 {
     use HasFactory, Notifiable;
     use LogsActivity;
