f301d1eacef366120b8fc6b84506fddd0781bee8
Freddy Heppell <freddy@freddyheppell.com>
2022-08-27 22:09:50 +0100

    Audit badges

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\View\Components\DonationBarComponent;
 use App\View\Components\NavBarComponent;
 use App\View\Components\PanelSideBarComponent;
 use Entities\Models\Eloquent\Account;
+use Entities\Models\Eloquent\Badge;
 use Entities\Models\Eloquent\BanAppeal;
 use Entities\Models\Eloquent\BuilderRankApplication;
 use Entities\Models\Eloquent\Donation;
@@ -72,6 +73,7 @@ final class AppServiceProvider extends ServiceProvider
             'donation_tier' => DonationTier::class,
             'builder_rank_application' => BuilderRankApplication::class,
             'ban_appeal' => BanAppeal::class,
+            'badge' => Badge::class,
         ]);
 
         Blade::component('navbar', NavBarComponent::class);
