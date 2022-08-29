479c0494136bf405812a8a526d842bd508855dd6
Andy Saw <andy-saw@hotmail.com>
2022-08-29 14:53:56 +0900

    Update

--- tests/Unit/App/Console/CleanupUnactivatedAccountsCommand_Test.php
+++ tests/Unit/App/Console/CleanupUnactivatedAccountsCommand_Test.php
@@ -1,22 +1,25 @@
 <?php
 
-namespace Tests;
+namespace Tests\Unit\App\Console;
 
-use App\Entities\Accounts\Models\Account;
+use Entities\Models\Eloquent\Account;
+use Entities\Models\Eloquent\AccountEmailChange;
+use Entities\Models\Eloquent\Group;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 final class CleanupUnactivatedAccountsCommand_Test extends TestCase
 {
     use RefreshDatabase;
 
-    public function testDeletesUnactivatedAccount()
+    public function test_deletes_unactivated_account()
     {
         $elapsedDaysToDelete = 14;
 
-        $accountToBeDeleted = factory(Account::class)->states('unactivated')->create([
+        $accountToBeDeleted = Account::factory()->unactivated()->create([
             'updated_at' => now()->subDays($elapsedDaysToDelete + 1),
         ]);
-        $accountJustCreated = factory(Account::class)->states('unactivated')->create([
+        $accountJustCreated = Account::factory()->unactivated()->create([
             'updated_at' => now(),
         ]);
 
@@ -35,6 +38,30 @@ final class CleanupUnactivatedAccountsCommand_Test extends TestCase
         ]);
         $this->assertDatabaseHas('accounts', [
             'account_id' => $accountJustCreated->getKey(),
+        ]);
+    }
+
+    public function test_cascade_deletes_account()
+    {
+        $account = Account::factory()
+            ->unactivated()
+            ->create(['updated_at' => now()->subDays(14)]);
+
+        $emailChange = AccountEmailChange::factory()
+            ->for($account)
+            ->create();
+
+        $group = Group::factory()->create();
+        $account->groups()->attach($group);
+
+        $this->artisan('cleanup:unactivated-accounts --days=7')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseMissing('accounts', [
+            'account_id' => $account->getKey(),
+        ]);
+        $this->assertDatabaseMissing(AccountEmailChange::getTableName(), [
+            'account_email_change_id' => $emailChange->getKey(),
         ]);
     }
 }
