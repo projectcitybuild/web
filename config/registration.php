c9c56dd98d9260244a0551c23fc77720e1f481e3
Andy Saw <andy-saw@hotmail.com>
2022-08-29 14:56:37 +0900

    Fix comment

--- config/registration.php
+++ config/registration.php
@@ -7,8 +7,8 @@ return [
     | Account cleanup
     |--------------------------------------------------------------------------
     |
-    | Any accounts with an unverified email address, older than the below number 
-    | of days will be automatically purged from storage.
+    | Any accounts with an unverified email address, older than the below number
+    | of days, will be automatically purged.
     |
     */
     'days_elapsed_until_unactivated_purge' => 14
