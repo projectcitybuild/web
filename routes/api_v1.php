f9bdd629c02a735c0d163e5cd34525280406d23b
Andy Saw <andy-saw@hotmail.com>
2022-09-09 12:39:40 +0900

    Update test suites

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\v1\GameBanV1Controller;
 use App\Http\Controllers\Api\v1\GroupApiController;
 use App\Http\Controllers\Api\v1\MinecraftAuthTokenController;
 use App\Http\Controllers\Api\v1\StripeWebhookController;
