26ea55a77bbdbd37cf62ee307bcc936cb9761185
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2022-10-08 23:54:26 +0900

    Add permanent redirects for all info pages (#707)
    * Add permanent redirects for all info pages
    
    * Lint

--- routes/web_redirects.php
+++ routes/web_redirects.php
@@ -2,9 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::permanentRedirect('terms', 'https://forums.projectcitybuild.com/t/community-rules/22928')->name('terms');
-Route::permanentRedirect('privacy', 'https://forums.projectcitybuild.com/privacy')->name('privacy');
-Route::permanentRedirect('wiki', 'https://wiki.projectcitybuild.com')->name('wiki');
-Route::permanentRedirect('maps', 'https://maps.pcbmc.co')->name('maps');
-Route::permanentRedirect('3d-maps', 'https://3d.pcbmc.co')->name('3d-maps');
-Route::permanentRedirect('report', 'https://forums.projectcitybuild.com/w/player-report')->name('report');
+Route::permanentRedirect('terms', 'https://forums.projectcitybuild.com/t/community-rules/22928')
+    ->name('terms');
+
+Route::permanentRedirect('privacy', 'https://forums.projectcitybuild.com/privacy')
+    ->name('privacy');
+
+Route::permanentRedirect('wiki', 'https://wiki.projectcitybuild.com')
+    ->name('wiki');
+
+Route::permanentRedirect('maps', 'https://maps.pcbmc.co')
+    ->name('maps');
+
+Route::permanentRedirect('3d-maps', 'https://3d.pcbmc.co')
+    ->name('3d-maps');
+
+Route::permanentRedirect('report', 'https://forums.projectcitybuild.com/w/player-report')
+    ->name('report');
+
+Route::permanentRedirect('rules', 'https://portal.projectcitybuild.com/books/rules/page/community-rules')
+    ->name('rules');
+
+Route::permanentRedirect('ranks', 'https://portal.projectcitybuild.com/books/player-ranks/page/list-of-ranks')
+    ->name('ranks');
+
+Route::permanentRedirect('staff', 'https://portal.projectcitybuild.com/books/list-of-staff/page/active')
+    ->name('staff');
+
+Route::permanentRedirect('vote', 'https://portal.projectcitybuild.com/books/server-voting/page/server-voting')
+    ->name('vote');
+
+Route::permanentRedirect('map-archive', 'https://portal.projectcitybuild.com/books/map-archive/page/map-archive')
+    ->name('map-archive');
