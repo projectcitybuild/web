38e0c8ca321076194a9e5a9e47c403b6a514a98c
andyksaw <andy-saw@hotmail.com>
2024-03-14 01:41:25 +0900

    Clean up

--- api/routes/api.php
+++ api/routes/api.php
@@ -25,7 +25,6 @@ use App\Http\Controllers\Minecraft\MinecraftConfigController;
 use App\Http\Controllers\Minecraft\MinecraftPlayerSyncController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware('guest')->group(function () {
     Route::post('register', [RegistrationController::class, 'register'])
         ->middleware('throttle:6,1');
@@ -34,29 +33,26 @@ Route::middleware('guest')->group(function () {
         ->name("account.update-email.confirm");
 
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-        ->middleware('guest')
         ->name('login');
 
     Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->middleware('guest')
         ->name('password.email');
 
     Route::post('/reset-password', [NewPasswordController::class, 'store'])
-        ->middleware('guest')
         ->name('password.store');
-
-    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['auth', 'signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware(['auth', 'throttle:6,1'])
-        ->name('verification.send');
-
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->middleware('auth')
-        ->name('logout');
 });
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::prefix('account')->group(function () {
