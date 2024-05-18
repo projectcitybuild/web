0fd4a0c806b337dd427f3e41193cc29badd8ad68
andyksaw <andy-saw@hotmail.com>
2024-05-18 15:51:15 +0900

    Fix foreign keys

--- api/database/migrations/2023_02_21_160805_v4.php
+++ api/database/migrations/2023_02_21_160805_v4.php
@@ -154,6 +154,7 @@ return new class extends Migration
         Schema::table('donation_perk', fn ($t) => $t->renameColumn('donation_perks_id', 'id'));
         Schema::table('donation_tier', fn ($t) => $t->renameColumn('donation_tier_id', 'id'));
         Schema::table('group', fn ($t) => $t->renameColumn('group_id', 'id'));
+        Schema::table('group_account_pivot', fn ($t) => $t->renameColumn('groups_accounts_id', 'id'));
         Schema::table('minecraft_auth_code', fn ($t) => $t->renameColumn('minecraft_auth_code_id', 'id'));
         Schema::table('payment', fn ($t) => $t->renameColumn('payment_id', 'id'));
         Schema::table('player', fn ($t) => $t->renameColumn('player_minecraft_id', 'id'));
@@ -164,27 +165,233 @@ return new class extends Migration
 
         Schema::table('minecraft_auth_code', fn ($t) => $t->renameColumn('player_minecraft_id', 'player_id'));
         Schema::table('player_alias', fn ($t) => $t->renameColumn('player_minecraft_id', 'player_id'));
+        Schema::table('ban_appeal', fn ($t) => $t->renameColumn('decider_player_minecraft_id', 'decider_player_id'));
+        Schema::table('ban_appeal', fn ($t) => $t->renameColumn('game_ban_id', 'player_ban_id'));
+
+
+        // Drop foreign keys
+
+        Schema::table('account_balance_transaction', function (Blueprint $table) {
+            $table->dropForeign('account_balance_transactions_account_id_foreign');
+            $table->dropIndex('account_balance_transactions_account_id_foreign');
+        });
+        Schema::table('account_email_change', function (Blueprint $table) {
+            $table->dropForeign('account_email_changes_account_id_foreign');
+            $table->dropIndex('account_email_changes_account_id_foreign');
+        });
+        Schema::table('ban_appeal', function (Blueprint $table) {
+            $table->dropForeign('ban_appeals_game_ban_id_foreign');
+            $table->dropIndex('ban_appeals_game_ban_id_foreign');
+
+            $table->dropForeign('ban_appeals_decider_player_minecraft_id_foreign');
+            $table->dropIndex('ban_appeals_decider_player_minecraft_id_foreign');
+        });
+        Schema::table('donation', function (Blueprint $table) {
+            $table->dropForeign('donations_account_id_foreign');
+            $table->dropIndex('donations_account_id_foreign');
+        });
+        Schema::table('donation_perk', function (Blueprint $table) {
+            $table->dropForeign('donation_perks_account_id_foreign');
+            $table->dropIndex('donation_perks_account_id_foreign');
+
+            $table->dropForeign('donation_perks_donation_id_foreign');
+            $table->dropIndex('donation_perks_donation_id_foreign');
+
+            $table->dropForeign('donation_perks_donation_tier_id_foreign');
+            $table->dropIndex('donation_perks_donation_tier_id_foreign');
+        });
+        Schema::table('group_account_pivot', function (Blueprint $table) {
+            $table->dropForeign('groups_accounts_group_id_foreign');
+            $table->dropIndex('groups_accounts_group_id_foreign');
+
+            $table->dropForeign('groups_accounts_account_id_foreign');
+            $table->dropIndex('groups_accounts_account_id_foreign');
+        });
+        Schema::table('ip_ban', function (Blueprint $table) {
+            $table->dropForeign('game_ip_bans_unbanner_player_id_foreign');
+            $table->dropIndex('game_ip_bans_unbanner_player_id_foreign');
+        });
+        Schema::table('payment', function (Blueprint $table) {
+            $table->dropForeign('payments_account_id_foreign');
+            $table->dropIndex('payments_account_id_foreign');
+        });
+        Schema::table('player', function (Blueprint $table) {
+            $table->dropForeign('players_minecraft_account_id_foreign');
+            $table->dropIndex('players_minecraft_account_id_foreign');
+        });
+        Schema::table('player_alias', function (Blueprint $table) {
+            $table->dropForeign('players_minecraft_aliases_player_minecraft_id_foreign');
+            $table->dropIndex('players_minecraft_aliases_player_minecraft_id_foreign');
+        });
+        Schema::table('player_ban', function (Blueprint $table) {
+            $table->dropForeign('game_network_bans_unbanner_player_id_foreign');
+            $table->dropIndex('game_network_bans_unbanner_player_id_foreign');
+        });
+        Schema::table('stripe_product', function (Blueprint $table) {
+            $table->dropForeign('stripe_products_donation_tier_id_foreign');
+            $table->dropIndex('stripe_products_donation_tier_id_foreign');
+        });
+
+
+        // Convert old id columns to bigint (the new default of `id()`)
+
+        Schema::table('account', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+        });
+        Schema::table('account_balance_transaction', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('account_email_change', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('badge', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+        });
+        Schema::table('badge_pivot', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('account_id')->change();
+            $table->unsignedBigInteger('badge_id')->change();
+        });
+        Schema::table('ban_appeal', function (Blueprint $table) {
+            $table->unsignedBigInteger('player_ban_id')->change();
+            $table->unsignedBigInteger('decider_player_id')->change();
+        });
+        Schema::table('builder_rank_application', function (Blueprint $table) {
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('donation', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('donation_perk', function (Blueprint $table) {
+            $table->unsignedBigInteger('donation_id')->change();
+            $table->unsignedBigInteger('donation_tier_id')->change();
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('donation_tier', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+        });
+        Schema::table('group', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+        });
+        Schema::table('group_account_pivot', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('group_id')->change();
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('ip_ban', function (Blueprint $table) {
+            $table->unsignedBigInteger('banner_player_id')->change();
+            $table->unsignedBigInteger('unbanner_player_id')->nullable()->change();
+        });
+        Schema::table('minecraft_auth_code', function (Blueprint $table) {
+            $table->unsignedBigInteger('player_id')->change();
+        });
+        Schema::table('payment', function (Blueprint $table) {
+            $table->unsignedBigInteger('account_id')->change();
+        });
+        Schema::table('player', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('account_id')->nullable()->change();
+        });
+        Schema::table('player_alias', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('player_id')->change();
+        });
+        Schema::table('player_ban', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('banned_player_id')->change();
+            $table->unsignedBigInteger('banner_player_id')->nullable()->change();
+            $table->unsignedBigInteger('unbanner_player_id')->nullable()->change();
+        });
+        Schema::table('player_warning', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement()->change();
+            $table->unsignedBigInteger('warned_player_id')->change();
+            $table->unsignedBigInteger('warner_player_id')->nullable()->change();
+        });
+        Schema::table('stripe_product', function (Blueprint $table) {
+            $table->unsignedBigInteger('donation_tier_id')->change();
+        });
+
+        // Restore foreign keys
+
+        Schema::table('account_balance_transaction', function (Blueprint $table) {
+            $table->foreignId('account_id')->change();
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('account_email_change', function (Blueprint $table) {
+            $table->foreignId('account_id')->change();
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('badge_pivot', function (Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('account');
+            $table->foreign('badge_id')->references('id')->on('badge');
+        });
+        Schema::table('ban_appeal', function (Blueprint $table) {
+            $table->foreign('player_ban_id')->references('id')->on('player_ban');
+            $table->foreign('decider_player_id')->references('id')->on('player');
+        });
+        Schema::table('builder_rank_application', function (Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('donation', function (Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('donation_perk', function (Blueprint $table) {
+            $table->foreign('donation_id')->references('id')->on('donation');
+            $table->foreign('donation_tier_id')->references('id')->on('donation_tier');
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('group_account_pivot', function (Blueprint $table) {
+            $table->foreign('group_id')->references('id')->on('group');
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('ip_ban', function (Blueprint $table) {
+            $table->foreign('banner_player_id')->references('id')->on('player');
+            $table->foreign('unbanner_player_id')->references('id')->on('player');
+        });
+        Schema::table('minecraft_auth_code', function (Blueprint $table) {
+            $table->foreign('player_id')->references('id')->on('player');
+        });
+        Schema::table('payment', function (Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('player', function (Blueprint $table) {
+            $table->foreign('account_id')->references('id')->on('account');
+        });
+        Schema::table('player_alias', function (Blueprint $table) {
+            $table->foreign('player_id')->references('id')->on('player');
+        });
+        Schema::table('player_ban', function (Blueprint $table) {
+            $table->foreign('banned_player_id')->references('id')->on('player');
+            $table->foreign('banner_player_id')->references('id')->on('player');
+            $table->foreign('unbanner_player_id')->references('id')->on('player');
+        });
+        Schema::table('player_warning', function (Blueprint $table) {
+            $table->foreign('warned_player_id')->references('id')->on('player');
+            $table->foreign('warner_player_id')->references('id')->on('player');
+        });
+        Schema::table('stripe_product', function (Blueprint $table) {
+            $table->foreign('donation_tier_id')->references('id')->on('donation_tier');
+        });
+
 
         // Introduce attributes
 
-//        Schema::create('attribute', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('name');
-//        });
-//
+        Schema::create('attribute', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
 //        Schema::create('group_attribute', function (Blueprint $table) {
 //            $table->id();
-//            $table->foreignId('group_id')->constrained(
-//                table: 'group',
-//                column: 'group_id',
-//            );
-//            $table->foreignId('attribute_id')->constrained(
-//                table: 'attribute',
-//            );
+//            $table->foreignId('group_id')->constrained();
+//            $table->foreignId('attribute_id')->constrained();
 //            $table->string('value');
 //            $table->timestamps();
 //        });
-//
+
 //        Schema::create('perk', function (Blueprint $table) {
 //            $table->id();
 //            $table->foreignId('player_id')->constrained(
