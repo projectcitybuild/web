e7e196f4f22a480e209fe8949b513d2ceaf5907e
andyksaw <andy-saw@hotmail.com>
2024-09-15 03:35:39 +0900

    Create new ban list layout

--- resources/views/front/components/button.blade.php
+++ resources/views/front/components/button.blade.php
@@ -1,23 +1,29 @@
 @props([
     'variant' => 'filled',
     'type' => 'button',
+    'size' => 'lg',
     'href',
 ])
 
 @php
     if ($variant == 'filled') {
-        $class = 'text-gray-50 bg-gray-900 rounded-md shadow-lg hover:bg-gray-800 active:bg-gray-700';
+        $class = ['text-gray-50', 'bg-gray-900', 'rounded-md', 'shadow-lg', 'hover:bg-gray-800', 'active:bg-gray-700'];
     } else if ($variant == 'outlined') {
-        $class = 'text-gray-900 border-gray-900 border-2 rounded-md shadow-lg hover:border-gray-500 active:bg-gray-200';
+        $class = ['text-gray-900', 'border-gray-900', 'border-2', 'rounded-md', 'shadow-lg', 'hover:border-gray-500', 'active:bg-gray-200'];
     }
-    $class = 'text-sm sm:text-lg px-6 py-4 cursor-pointer flex justify-center items-center gap-2 ' . $class;
+    if ($size == 'lg') {
+        array_push($class, 'py-4', 'px-6', 'text-sm', 'sm:text-lg', );
+    } else {
+        array_push($class, 'py-3', 'px-4', 'text-sm', 'sm:text-md', );
+    }
+    array_push($class, 'cursor-pointer', 'flex', 'justify-center', 'items-center', 'gap-2');
 @endphp
 
 @if (empty($href))
     <button
         {{ $attributes->merge([
             'type' => $type,
-            'class' => $class,
+            'class' => implode(separator: ' ', array: $class),
         ]) }}
     >
         {{ $slot }}
--- resources/views/front/pages/banlist.blade.php
+++ resources/views/front/pages/banlist.blade.php
@@ -1,65 +1,68 @@
-@extends('front.templates.master')
+@extends('front.layouts.root-layout')
 
 @section('title', 'Player Ban List')
 @section('description', 'Players listed on this page are currently banned on one or more servers on our game network')
 
 @section('body')
-    <header class="image-header">
-        <div class="container">
-            <h1>Ban List</h1>
-        </div>
-    </header>
+    <x-navbar />
 
-    <main class="page settings">
-        <div class="settings__section">
-            <table class="table table--divided">
+    <div class="flex flex-col p-9">
+        <h1 class="text-4xl font-bold">Banned Players</h1>
+        <div class="text-gray-500 mt-3">
+            List of players banned from our services.
+        </div>
+        <div class="text-gray-900 mt-3">
+            If you have been unfairly or incorrectly banned, please <a href="{{ route('front.appeal') }}" class="text-blue-500">submit an appeal</a>.
+        </div>
+    </div>
+
+    <div class="bg-gray-50 mx-2 md:mx-6 rounded-lg">
+        <div class="p-6 border-b border-gray-200">
+            <form action="{{ route('front.banlist') }}" method="get">
+                <input
+                    type="text"
+                    class="rounded-md bg-gray-100 border-gray-200 text-sm"
+                    name="query"
+                    value="{{ $query }}"
+                    placeholder="Search..."
+                >
+            </form>
+        </div>
+
+        <div class="p-6">
+            <table class="w-full overflow-x-auto">
                 <thead>
-                <tr>
-                    <th colspan="5">
-                        <form action="{{ route('front.banlist') }}" method="get">
-                            <input type="text" class="input-text" name="query" value="{{ $query }}" placeholder="Search banned player or reason">
-                        </form>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Banned Player</th>
-                    <th>Reason</th>
-                    <th>Banned By</th>
-                    <th>Banned At</th>
-                    <th>Expires</th>
-                </tr>
+                    <tr class="text-left text-sm text-gray-500 bg-gray-100">
+                        <th class="py-4 px-2">Player Name</th>
+                        <th class="py-4 px-2">Reason</th>
+                        <th class="py-4 px-2">Ban Date</th>
+                        <th class="py-4 px-2">Expiry Date</th>
+                    </tr>
                 </thead>
                 <tbody>
                 @forelse($bans as $ban)
                     <tr>
-                        <td>
-                            <img src="https://minotar.net/avatar//{{ $ban->bannedPlayer->uuid }}/16" class="banlist__head" alt="">
-                            {{ $ban->banned_alias_at_time }}
+                        <td class="py-4 px-2 flex gap-2">
+                            <img src="https://minotar.net/avatar//{{ $ban->bannedPlayer->uuid }}/16" class="rounded-md h-6">
+                            <span class="font-bold">{{ $ban->banned_alias_at_time }}</span>
                         </td>
-                        <td>
+                        <td class="py-4 px-2">
                             @if ($ban->reason != null)
                                 {{ $ban->reason }}
                             @else
                                 -
                             @endif
                         </td>
-                        <td>
-                            @if($ban->banner_player_id != null && count($ban->bannerPlayer->aliases) > 0)
-                                <img src="https://minotar.net/avatar/{{ $ban->bannerPlayer->uuid }}/16" class="banlist__head" alt="">
-                                {{ $ban->bannerPlayer->getBanReadableName() }}
-                            @else
-                                -
-                            @endif
-                        </td>
-                        <td>
+                        <td class="py-4 px-2">
                             {{ $ban->created_at->format('j M Y H:i') }}
                         </td>
-                        <td>
-                            @if ($ban->expires_at != null)
-                                {{ $ban->created_at->format('j M Y H:i') }}
-                            @else
-                                -
-                            @endif
+                        <td class="py-4 px-2">
+                            <x-button size="sm">
+                                View Details
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                    <path fill-rule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                                </svg>
+                            </x-button>
                         </td>
                     </tr>
                 @empty
@@ -69,10 +72,8 @@
                 @endforelse
                 </tbody>
             </table>
-
-            {{ $bans->links('vendor.pagination.default') }}
         </div>
-    </main>
 
-    @include('front.components.footer')
+        {{ $bans->links('vendor.pagination.default') }}
+    </div>
 @endsection
