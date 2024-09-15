0f9d438549f265f2c8bf863d804a93dc6401b04d
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2024-09-15 14:12:11 +0000

    feat: add badges to dashboard (#791)

--- app/Http/Controllers/Front/Account/AccountProfileController.php
+++ app/Http/Controllers/Front/Account/AccountProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front\Account;
 
 use App\Http\Controllers\WebController;
+use App\Models\Badge;
 use Illuminate\Http\Request;
 
 class AccountProfileController extends WebController
@@ -10,7 +11,10 @@ class AccountProfileController extends WebController
     public function show(Request $request)
     {
         $account = $request->user();
+        $badges = Badge::orderBy('display_name', 'asc')
+            ->get();
 
-        return view('front.pages.account.profile')->with(compact('account'));
+        return view('front.pages.account.profile')
+            ->with(compact('account', 'badges'));
     }
 }
--- resources/views/admin/badges/_form.blade.php
+++ resources/views/admin/badges/_form.blade.php
@@ -11,6 +11,22 @@
     </div>
 </div>
 <div class="row mb-3">
+    <label for="ip" class="col-sm-3 col-form-label horizontal-label">Hidden in Badge List</label>
+    <div class="col-sm-9">
+        <input
+            type="checkbox"
+            name="list_hidden"
+            class="
+                rounded-sm
+                bg-gray-300 border-0
+                checked:bg-gray-900
+            "
+            value="1"
+            @if(old('list_hidden', $badge->list_hidden) == 1) checked @endif
+        >
+    </div>
+</div>
+<div class="row mb-3">
     <div class="col-sm-9 ms-auto">
         <button type="submit" class="btn btn-primary">{{ $buttonText }}</button>
     </div>
