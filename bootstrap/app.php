ddcb515c590e5923eaa75dadfeb288602d9910a5
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2024-09-16 09:51:32 +0000

    feat: log API calls for debugging (#796)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\VerifyCsrfToken::class,
         ]);
+        $middleware->api(append: [
+           \App\Http\Middleware\LogApiCalls::class,
+        ]);
         $middleware->redirectGuestsTo(
             fn (Request $request) => route('front.login'),
         );
