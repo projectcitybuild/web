cccfe117b68cf37e8f4ce94ee61894ca4498b84e
andyksaw <andy-saw@hotmail.com>
2024-10-28 22:36:48 +0900

    Allow decimals

--- app/Http/Controllers/Api/v2/Minecraft/MinecraftWarpController.php
+++ app/Http/Controllers/Api/v2/Minecraft/MinecraftWarpController.php
@@ -20,9 +20,9 @@ final class MinecraftWarpController extends ApiController
         $request->validate([
             'name' => ['required', 'string', 'alpha_dash', Rule::unique('minecraft_warps')],
             'world' => 'required|string',
-            'x' => 'required|integer',
-            'y' => 'required|integer',
-            'z' => 'required|integer',
+            'x' => 'required|numeric',
+            'y' => 'required|numeric',
+            'z' => 'required|numeric',
             'pitch' => 'required|numeric',
             'yaw' => 'required|numeric',
         ]);
@@ -35,9 +35,9 @@ final class MinecraftWarpController extends ApiController
         $request->validate([
             'name' => ['required', 'string', 'alpha_dash', Rule::unique('minecraft_warps')->ignore($warp)],
             'world' => 'required|string',
-            'x' => 'required|integer',
-            'y' => 'required|integer',
-            'z' => 'required|integer',
+            'x' => 'required|numeric',
+            'y' => 'required|numeric',
+            'z' => 'required|numeric',
             'pitch' => 'required|numeric',
             'yaw' => 'required|numeric',
         ]);
