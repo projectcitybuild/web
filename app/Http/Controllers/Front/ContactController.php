af95f705926ede232134c7ba3497a3e8b92079b0
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2024-11-10 07:40:11 +0000

    feat: general contact form (#837)

---
+++ app/Http/Controllers/Front/ContactController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Domains\Contact\Notifications\ContactNotification;
+use App\Http\Controllers\WebController;
+use App\Http\Requests\ContactRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Notification;
+
+final class ContactController extends WebController
+{
+    public function index()
+    {
+        return view('front.pages.contact');
+    }
+
+    public function store(ContactRequest $request)
+    {
+        $validated = $request->validated();
+
+        $notification = new ContactNotification(
+            name: $validated['name'],
+            email: $validated['email'],
+            subject: $validated['subject'],
+            inquiry: $validated['inquiry'],
+            ip: $request->ip(),
+            userAgent: $request->userAgent(),
+        );
+
+        $discordChannel = config('discord.webhook_contact_channel');
+        throw_if (empty($discordChannel), 'No discord channel set for contact form');
+        Notification::route('discord', $discordChannel)
+            ->notify($notification);
+
+        $email = config('mail.admin_email');
+        throw_if (empty($email), 'No admin email set for contact form');
+        Notification::route('mail', $email)
+            ->notify($notification);
+
+        return redirect()
+            ->route('front.contact')
+            ->with(['success' => 'Your inquiry has been submitted. Thank you']);
+    }
+}
--- config/discord.php
+++ config/discord.php
@@ -3,6 +3,7 @@
 return [
     'webhook_architect_channel' => env('DISCORD_WEBHOOK_ARCHITECT'),
     'webhook_ban_appeal_channel' => env('DISCORD_WEBHOOK_BAN_APPEAL'),
+    'webhook_contact_channel' => env('DISCORD_WEBHOOK_CONTACT'),
 
     'invite_url' => 'https://discord.gg/3NYaUeScDX',
 ];
