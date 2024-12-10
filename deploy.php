0b21febb6d1d84c0297d7d3d131266f3c40fe568
andyksaw <andy-saw@hotmail.com>
2024-12-10 18:23:52 +0900

    Add deployment workflow

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'git@github.com:projectcitybuild/web.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host(getenv('DEPLOY_HOST'))
+    ->set('remote_user', getenv('DEPLOY_USER'))
+    ->set('deploy_path', getenv('DEPLOY_PATH'));
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
