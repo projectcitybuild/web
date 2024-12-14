13429e85f81f606e966374768127c7135a5ebc5b
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2024-12-14 17:34:00 +0900

    ci: notify discord of deployment events (#848)

--- deploy.php
+++ deploy.php
@@ -3,41 +3,52 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/discord.php';
 
-// Config
+function configure(): void
+{
+    set('repository', 'https://github.com/projectcitybuild/web.git');
 
-set('repository', 'https://github.com/projectcitybuild/web.git');
+    add('shared_files', []);
+    add('shared_dirs', []);
+    add('writable_dirs', []);
 
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+    host(getenv('DEPLOY_HOST'))
+        ->set('remote_user', getenv('DEPLOY_USER'))
+        ->set('deploy_path', getenv('DEPLOY_PATH'))
+        ->set('branch', getenv('DEPLOY_BRANCH'))
+        ->set('discord_channel', getenv('DEPLOY_DISCORD_CHANNEL_ID'))
+        ->set('discord_token', getenv('DEPLOY_DISCORD_CHANNEL_TOKEN'));
 
-// Hosts
+    after('deploy:failed', 'deploy:unlock');
+}
 
-host(getenv('DEPLOY_HOST'))
-    ->set('remote_user', getenv('DEPLOY_USER'))
-    ->set('deploy_path', getenv('DEPLOY_PATH'))
-    ->set('branch', getenv('DEPLOY_BRANCH'));
+function setupDiscordTasks(): void
+{
+    before('deploy', 'discord:notify');
+    after('deploy:success', 'discord:notify:success');
+    after('deploy:failed', 'discord:notify:failure');
+}
 
-// Hooks
+function setupFrontendTasks(): void
+{
+    // The Laravel recipe doesn't build any frontend assets, so we need
+    // to run our frontend tasks after dependencies are installed
+    after('deploy:vendors', 'deploy:frontend');
 
-after('deploy:failed', 'deploy:unlock');
+    // NVM doesn't play well with deployer
+    // https://github.com/deployphp/deployer/issues/2535
+    //
+    // Every command related tp npm must call `use_nvm` so that it's
+    // sourced in the run command's shell
+    set('use_nvm', 'source $HOME/.nvm/nvm.sh');
 
-// Additional tasks
+    task('deploy:frontend', function () {
+        run('{{use_nvm}} && cd {{release_path}} && npm ci');
+        run('{{use_nvm}} && cd {{release_path}} && npm run build');
+    })->desc('Build frontend assets');
+}
 
-// The Laravel recipe doesn't build any frontend assets, so we'll
-// need to do it ourselves here
-
-// NVM doesn't play well with deployer
-// https://github.com/deployphp/deployer/issues/2535
-//
-// Must call use_nvm in every run command related to npm so that
-// it's sourced in the current run's shell
-set('use_nvm', 'source $HOME/.nvm/nvm.sh');
-
-task('deploy:frontend', function () {
-    run('{{use_nvm}} && cd {{release_path}} && npm ci');
-    run('{{use_nvm}} && cd {{release_path}} && npm run build');
-})->desc('Build frontend assets');
-
-after('deploy:vendors', 'deploy:frontend');
+configure();
+setupDiscordTasks();
+setupFrontendTasks();
