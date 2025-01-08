02dce120707c3be71827def426c0ad86320032e0
andyksaw <andy-saw@hotmail.com>
2025-01-08 13:40:14 +1100

    Add empty message

--- resources/js/manage/Pages/Accounts/Partials/AccountPlayersTable.vue
+++ resources/js/manage/Pages/Accounts/Partials/AccountPlayersTable.vue
@@ -13,7 +13,7 @@ defineEmits(['unlinkTap'])
 </script>
 
 <template>
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table v-if="players.length > 0" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
             <th scope="col" class="px-4 py-3">Id</th>
@@ -49,4 +49,7 @@ defineEmits(['unlinkTap'])
         </tr>
         </tbody>
     </table>
+    <div v-else class="flex p-4">
+        <span class="text-sm text-gray-400">No linked players</span>
+    </div>
 </template>
