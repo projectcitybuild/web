7dfa868b160c8de18802d02104ed4af57778bb6d
andyksaw <andy-saw@hotmail.com>
2025-01-21 00:10:40 +0900

    Update introduction

--- resources/views/front/pages/home/index.blade.php
+++ resources/views/front/pages/home/index.blade.php
@@ -9,30 +9,64 @@ One of the world's longest-running Minecraft servers; we're a community of creat
     <x-front::hero />
 
     <main>
-        <section class="introduction">
-            <div class="max-w-screen-2xl mx-auto px-6 md:px-12">
-                <h1>Minecraft 24/7</h1>
-
-                <div class="introduction__content">
-                    <div class="introduction__text">
-                        We're a Minecraft community that's been around since October 2010.<p />
-
-                        With our free-build Creative and Survival multiplayer maps, we offer a fun platform & building experience like no other. You can visit and build in established towns & cities or start your own.
+        <section class="bg-white">
+            <div class="max-w-screen-2xl mx-auto p-12">
+                <div class="flex flex-col gap-4 md:flex-row md:gap-12 md:py-8">
+                    <div class="mb-4 text-4xl font-light leading-relaxed">
+                        We're a Minecraft server and community that's been around <strong class="font-bold">since October 2010</strong>.
                     </div>
 
-                    <div class="introduction__points">
-                        <ul>
-                            <li><i class="bullet fas fa-cube"></i> Free-build maps</li>
-                            <li><i class="bullet fas fa-cube"></i> Grief protection</li>
-                            <li><i class="bullet fas fa-cube"></i> Friendly community</li>
-                        </ul>
-                        <ul>
-                            <li><i class="bullet fas fa-cube"></i> Earn trust & skill based perks</li>
-                            <li><i class="bullet fas fa-cube"></i> Helpful staff</li>
-                            <li><i class="bullet fas fa-cube"></i> Events and competitions</li>
-                        </ul>
+                    <div class="text-gray-600 text-lg leading-loose pt-3">
+                        With our free-build Creative and Survival multiplayer worlds, we offer a fun platform & building
+                        experience like no other. You can visit and build in established towns & cities or start your own.
                     </div>
                 </div>
+
+                <ul class="mt-12 grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 8v4l3 3M3.22302 14C4.13247 18.008 7.71683 21 12 21c4.9706 0 9-4.0294 9-9 0-4.97056-4.0294-9-9-9-3.72916 0-6.92858 2.26806-8.29409 5.5M7 9H3V5"/>
+                        </svg>
+
+                        <h2 class="mt-4 text-4xl font-display">Minecraft 24/7</h2>
+                        <span class="text-sm text-gray-600">Online all day, every day since the beginning</span>
+                    </li>
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"/>
+                        </svg>
+                        <h2 class="mt-4 text-4xl font-display">Free-Build Worlds</h2>
+                        <span class="text-sm text-gray-600">TODO</span>
+                    </li>
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.5 11.5 11 13l4-3.5M12 20a16.405 16.405 0 0 1-5.092-5.804A16.694 16.694 0 0 1 5 6.666L12 4l7 2.667a16.695 16.695 0 0 1-1.908 7.529A16.406 16.406 0 0 1 12 20Z"/>
+                        </svg>
+                        <h2 class="mt-4 text-4xl font-display">Grief Protection</h2>
+                        <span class="text-sm text-gray-600">TODO</span>
+                    </li>
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                        </svg>
+                        <h2 class="mt-4 text-4xl font-display">Community Focussed</h2>
+                        <span class="text-sm text-gray-600">TODO</span>
+                    </li>
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 9h.01M8.99 9H9m12 3a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM6.6 13a5.5 5.5 0 0 0 10.81 0H6.6Z"/>
+                        </svg>
+                        <h2 class="mt-4 text-4xl font-display">Trust & Build Ranks</h2>
+                        <span class="text-sm text-gray-600">TODO</span>
+                    </li>
+                    <li class="rounded-lg bg-gray-50 p-6 text-lg">
+                        <svg class="size-12 text-gray-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m7.171 12.906-2.153 6.411 2.672-.89 1.568 2.34 1.825-5.183m5.73-2.678 2.154 6.411-2.673-.89-1.568 2.34-1.825-5.183M9.165 4.3c.58.068 1.153-.17 1.515-.628a1.681 1.681 0 0 1 2.64 0 1.68 1.68 0 0 0 1.515.628 1.681 1.681 0 0 1 1.866 1.866c-.068.58.17 1.154.628 1.516a1.681 1.681 0 0 1 0 2.639 1.682 1.682 0 0 0-.628 1.515 1.681 1.681 0 0 1-1.866 1.866 1.681 1.681 0 0 0-1.516.628 1.681 1.681 0 0 1-2.639 0 1.681 1.681 0 0 0-1.515-.628 1.681 1.681 0 0 1-1.867-1.866 1.681 1.681 0 0 0-.627-1.515 1.681 1.681 0 0 1 0-2.64c.458-.361.696-.935.627-1.515A1.681 1.681 0 0 1 9.165 4.3ZM14 9a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                        </svg>
+                        <h2 class="mt-4 text-4xl font-display">Events and Competitions</h2>
+                        <span class="text-sm text-gray-600">TODO</span>
+                    </li>
+                </ul>
             </div>
         </section>
 
