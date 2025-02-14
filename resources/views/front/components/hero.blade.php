07f760f07d37d6e9e628908a9c20e943adf9284b
andyksaw <andy-saw@hotmail.com>
2025-02-14 20:25:41 +0900

    Fix missing icon

--- resources/views/front/components/hero.blade.php
+++ resources/views/front/components/hero.blade.php
@@ -29,8 +29,13 @@
                     bg-amber-500 rounded-md shadow-lg
                     hover:bg-amber-400
                     px-6 py-4
+                    flex flex-row items-center
+                    space-x-1
                 ">
-                    Connect to
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                    </svg>
+                    <span>Connect to</span>
                     <span class="font-bold">
                         <i class="fas fa-copy"></i> <u>pcbmc.co</u>
                     </span>
