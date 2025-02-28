ca6afcd01eb299471923850582e74d2b95f72c09
andyksaw <andy-saw@hotmail.com>
2025-02-28 19:47:42 +0900

    Track term acceptance at registration

---
+++ database/migrations/2025_02_28_103847_update_account_terms.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->boolean('terms_accepted')->default(true);
+        });
+
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->boolean('terms_accepted')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('terms_accepted');
+        });
+    }
+};
