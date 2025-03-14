a4d97d2313798890d728d5398d6c36c372d5f363
andyksaw <andy-saw@hotmail.com>
2025-03-14 16:57:16 +0900

    Only prune telescope if enabled

--- app/Core/Support/Passport/PassportServiceProvider.php
+++ app/Core/Support/Passport/PassportServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Core\Support\Passport;
 
 use App\Http\Controllers\Api\OAuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\ServiceProvider;
 
 final class PassportServiceProvider extends ServiceProvider
@@ -14,5 +15,10 @@ final class PassportServiceProvider extends ServiceProvider
             Route::get('oauth/me', [OAuthController::class, 'show'])
                 ->middleware('auth:api');
         }
+
+        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+            $schedule::command('passport:purge')
+                ->hourly();
+        });
     }
 }
--- app/Core/Support/Telescope/TelescopeServiceProvider.php
+++ app/Core/Support/Telescope/TelescopeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Core\Support\Telescope;
 
 use App\Core\Domains\Environment\EnvironmentLevel;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Telescope\TelescopeServiceProvider as LaravelTelescopeServiceProvider;
 
@@ -10,12 +11,24 @@ final class TelescopeServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if (! $this->enabled()) return;
+
+        $this->app->register(LaravelTelescopeServiceProvider::class);
+        $this->app->register(InternalTelescopeServiceProvider::class);
+
+        $this->app->afterResolving(Schedule::class, function (Schedule $schedule) {
+            $schedule::command('telescope:prune')
+                ->daily();
+        });
+    }
+
+    private function enabled(): bool
+    {
         $isLocalEnv = $this->app->environment(EnvironmentLevel::DEVELOPMENT->value);
 
         // Not installed on production environments
-        if ($isLocalEnv && class_exists(LaravelTelescopeServiceProvider::class)) {
-            $this->app->register(LaravelTelescopeServiceProvider::class);
-            $this->app->register(InternalTelescopeServiceProvider::class);
-        }
+        $isInstalled = class_exists(LaravelTelescopeServiceProvider::class);
+
+        return $isLocalEnv && $isInstalled;
     }
 }
