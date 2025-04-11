2edf4003b7e58cb4091b9c4e16a350d2610701d1
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2025-04-11 17:33:43 +0900

    fix: expand map iFrame to full height (#914)

--- resources/views/front/pages/maps.blade.php
+++ resources/views/front/pages/maps.blade.php
@@ -4,6 +4,10 @@
 
 @push('head')
     <style>
+        html, body {
+            height: 100%;
+            min-height: 100%;
+        }
         #app {
             width: 100%;
             height: 100%;
@@ -18,7 +22,8 @@
         <iframe
             class="size-full flex-grow"
             src="https://maps.pcbmc.co"
-            style="flex-grow: 1">
+            style="flex-grow: 1; border: 0">
+            Your browser does not support or has disabled iFrames
         </iframe>
     </div>
 @endsection
