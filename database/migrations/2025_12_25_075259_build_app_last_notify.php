0d3fcfef749d5e27fdd1d5e57f56eadee464bafe
andyksaw <andy-saw@hotmail.com>
2025-12-25 22:05:14 +1100

    Migrate existing

--- database/migrations/2025_12_25_075259_build_app_last_notify.php
+++ database/migrations/2025_12_25_075259_build_app_last_notify.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\BuilderRankApplication;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,6 +15,12 @@ return new class extends Migration
         Schema::table('builder_rank_applications', function (Blueprint $table) {
             $table->dateTime('next_reminder_at')->nullable()->after('closed_at');
         });
+
+        $openApps = BuilderRankApplication::where('closed_at', null)->get();
+        foreach ($openApps as $app) {
+            $app->next_reminder_at = $app->created_at->copy()->addDays(7);
+            $app->save();
+        }
     }
 
     /**
