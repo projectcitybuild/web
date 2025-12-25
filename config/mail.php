fbb575f975d604debaed142cb89708e0a862aa0a
Andy Saw <7236312+andyksaw@users.noreply.github.com>
2025-12-25 19:48:34 +1100

    feat: add dev container to continuously run queue (#970)

--- config/mail.php
+++ config/mail.php
@@ -50,16 +50,8 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
-        'ses' => [
-            'transport' => 'ses',
-        ],
-
         'mailgun' => [
             'transport' => 'mailgun',
-        ],
-
-        'postmark' => [
-            'transport' => 'postmark',
         ],
 
         'sendmail' => [
