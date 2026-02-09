2e54ab7ff231ce7544ae0906d8696e4c8ea322a6
andyksaw <7236312+andyksaw@users.noreply.github.com>
2026-02-09 05:48:51 +0000

    Apply automatic changes

--- tests/Unit/App/Core/Domains/Discord/DiscordMessageTest.php
+++ tests/Unit/App/Core/Domains/Discord/DiscordMessageTest.php
@@ -2,24 +2,24 @@
 
 use App\Core\Domains\Discord\Data\DiscordMessage;
 
-describe("json", function () {
-    it("does not contain thread_name if null", function () {
-       $message = new DiscordMessage();
-       $json = $message->toJson();
-       expect($json['thread_name'])->toBeNull();
+describe('json', function () {
+    it('does not contain thread_name if null', function () {
+        $message = new DiscordMessage;
+        $json = $message->toJson();
+        expect($json['thread_name'])->toBeNull();
 
-       $message = new DiscordMessage(threadName: "test");
-       $json = $message->toJson();
-       expect($json['thread_name'])->toEqual("test");
+        $message = new DiscordMessage(threadName: 'test');
+        $json = $message->toJson();
+        expect($json['thread_name'])->toEqual('test');
     });
 
-    it("truncates thread_name over 100 characters", function () {
-       $message = new DiscordMessage(threadName: str_repeat("a", 100));
-       $json = $message->toJson();
-       expect($json['thread_name'])->toEqual(str_repeat("a", 100));
+    it('truncates thread_name over 100 characters', function () {
+        $message = new DiscordMessage(threadName: str_repeat('a', 100));
+        $json = $message->toJson();
+        expect($json['thread_name'])->toEqual(str_repeat('a', 100));
 
-       $message = new DiscordMessage(threadName: str_repeat("a", 101));
-       $json = $message->toJson();
-       expect($json['thread_name'])->toEqual(str_repeat("a", 97)."...");
+        $message = new DiscordMessage(threadName: str_repeat('a', 101));
+        $json = $message->toJson();
+        expect($json['thread_name'])->toEqual(str_repeat('a', 97).'...');
     });
 });
